a4925aae003893c83d167a4dd81a58dbd28e72f7
Carlos A. Gomes <carlos.algms@gmail.com>
2018-04-09 21:29:06 -0300

    split shell configuration into server and windows

--- shell/install.bat
+++ shell/install.bat
@@ -3,7 +3,7 @@ set "SCRIPT_DIR=%~dp0"
 
 mklink /D %HOME%\bin            %SCRIPT_DIR%\bin
 
-mklink    %HOME%\.bash_aliases  %SCRIPT_DIR%\aliases
+mklink    %HOME%\.bash_aliases  %SCRIPT_DIR%\aliases_common
 mklink    %HOME%\.bash_common   %SCRIPT_DIR%\bash_common
 mklink    %HOME%\.bash_profile  %SCRIPT_DIR%\profile
 mklink    %HOME%\.bashrc        %SCRIPT_DIR%\bashrc
--- shell/install.sh
+++ shell/install.sh
@@ -2,13 +2,15 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -snf $SCRIPT_DIR/aliases       $HOME/.bash_aliases
+ln -snf $SCRIPT_DIR/aliases_servers $HOME/.bash_aliases
+ln -snf $SCRIPT_DIR/aliases_common  $HOME/.aliases_common
+
 # bash_common is loaded by both profile and bashrc
-ln -snf $SCRIPT_DIR/bash_common   $HOME/.bash_common
+ln -snf $SCRIPT_DIR/bash_common     $HOME/.bash_common
 
-ln -snf $SCRIPT_DIR/profile       $HOME/.bash_profile
-ln -snf $SCRIPT_DIR/bashrc        $HOME/.bashrc
+ln -snf $SCRIPT_DIR/profile         $HOME/.bash_profile
+ln -snf $SCRIPT_DIR/bashrc          $HOME/.bashrc
 
-ln -snf $SCRIPT_DIR/minttyrc      $HOME/.minttyrc
+ln -snf $SCRIPT_DIR/minttyrc        $HOME/.minttyrc
 
-ln -snf $SCRIPT_DIR/bin           $HOME/bin
+ln -snf $SCRIPT_DIR/bin             $HOME/bin
