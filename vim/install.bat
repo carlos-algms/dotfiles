a134093a53bc4655d05e8912874ba85aaca2f782
Carlos A. Gomes <carlos.algms@gmail.com>
2018-04-09 21:40:58 -0300

    Added project configuration

--- vim/install.bat
+++ vim/install.bat
@@ -3,3 +3,6 @@ set "SCRIPT_DIR=%~dp0"
 
 mklink /d %HOME%\.vim       %SCRIPT_DIR%\vimdir
 mklink    %HOME%\.vimrc     %SCRIPT_DIR%\vimdir\vimrc
+
+attrib +h /l %HOME%\.vim
+attrib +h /l %HOME%\.vimrc
