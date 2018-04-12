c5d21d665c3604e4fc88ae86cc9d1a72e9541318
Carlos A. Gomes <carlos.algms@gmail.com>
2018-04-12 20:36:26 -0300

    Added extensions to better use IDE features

--- shell/install.bat
+++ shell/install.bat
@@ -3,11 +3,11 @@ set "SCRIPT_DIR=%~dp0"
 
 mklink /D %HOME%\bin            %SCRIPT_DIR%\bin
 
-mklink    %HOME%\.bash_aliases  %SCRIPT_DIR%\aliases_common
-mklink    %HOME%\.bash_common   %SCRIPT_DIR%\bash_common
-mklink    %HOME%\.bash_profile  %SCRIPT_DIR%\profile
+mklink    %HOME%\.bash_aliases  %SCRIPT_DIR%\aliases_common.sh
+mklink    %HOME%\.bash_common   %SCRIPT_DIR%\bash_common.sh
+mklink    %HOME%\.bash_profile  %SCRIPT_DIR%\bashrc
 mklink    %HOME%\.bashrc        %SCRIPT_DIR%\bashrc
-mklink    %HOME%\.minttyrc        %SCRIPT_DIR%\minttyrc
+mklink    %HOME%\.minttyrc      %SCRIPT_DIR%\minttyrc
 
 attrib +h /l %HOME%\.bash_aliases
 attrib +h /l %HOME%\.bash_common
--- shell/install.sh
+++ shell/install.sh
@@ -2,17 +2,17 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -snf $SCRIPT_DIR/aliases_servers $HOME/.bash_aliases
-ln -snf $SCRIPT_DIR/aliases_common  $HOME/.aliases_common
+ln -snf $SCRIPT_DIR/aliases_servers.sh  $HOME/.bash_aliases
+ln -snf $SCRIPT_DIR/aliases_common.sh   $HOME/.aliases_common
 
 # bash_common is loaded by both profile and bashrc
-ln -snf $SCRIPT_DIR/bash_common     $HOME/.bash_common
+ln -snf $SCRIPT_DIR/bash_common.sh      $HOME/.bash_common
 
-ln -snf $SCRIPT_DIR/profile         $HOME/.bash_profile
-ln -snf $SCRIPT_DIR/bashrc          $HOME/.bashrc
+ln -snf $SCRIPT_DIR/bashrc              $HOME/.bash_profile
+ln -snf $SCRIPT_DIR/bashrc              $HOME/.bashrc
 
-ln -snf $SCRIPT_DIR/minttyrc        $HOME/.minttyrc
+ln -snf $SCRIPT_DIR/minttyrc            $HOME/.minttyrc
 
-ln -snf $SCRIPT_DIR/bin             $HOME/bin
+ln -snf $SCRIPT_DIR/bin                 $HOME/bin
 
 chmod u+x $SCRIPT_DIR/bin/*
