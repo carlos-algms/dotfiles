f87ac4733e8684bcff0cb76416f3af8c44480d15
Carlos Gomes <carlos.gomes@thomsonreuters.com>
2018-05-02 08:40:47 -0300

    Using full paths to avoid use alias with customizations

--- shell/common/ssh_agent.sh
+++ shell/common/ssh_agent.sh
@@ -3,11 +3,11 @@ export SSH_ENV="$HOME/.ssh/environment"
 function start_agent {
     echo "Initialising new SSH agent..."
 
-    /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
+    /usr/bin/ssh-agent | /bin/sed 's/^echo/#echo/' > "${SSH_ENV}"
 
     echo "SSH agent started"
 
-    chmod 600 "${SSH_ENV}"
+    /bin/chmod 600 "${SSH_ENV}"
     . "${SSH_ENV}" > /dev/null
 
     /usr/bin/ssh-add;
--- shell/windows/bash/start_ssh_agent.sh
+++ shell/windows/bash/start_ssh_agent.sh
@@ -1,8 +1,8 @@
-
+echo ssh win
 # Source SSH settings, if applicable
 if [ -f "${SSH_ENV}" ]; then
     . "${SSH_ENV}" > /dev/null
-    ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || { start_agent; }
+    /bin/ps -ef | grep ${SSH_AGENT_PID} | /bin/grep ssh-agent$ > /dev/null || { start_agent; }
 else
     start_agent;
 fi
