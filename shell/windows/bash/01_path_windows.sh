86b39a31ddd900ad7b2749cde6f3ceaea8bbfa4f
Carlos Gomes <carlos.gomes@thomsonreuters.com>
2018-05-24 14:47:49 -0300

    Fix windows clean path

--- shell/windows/bash/01_path_windows.sh
+++ shell/windows/bash/01_path_windows.sh
@@ -1,6 +1,8 @@
 
 export ORIGINAL_PATH="$PATH"
-cleanPath="/mingw64/bin:/usr/local/bin:/usr/bin:/bin:/c/Windows/system32:/c/Windows:/c/Windows/System32/WindowsPowerShell/v1.0:${HOME}/AppData/Roaming/nvm:/c/Program Files/nodejs:/d/xampp-7.1.1/php:/d/xampp-7.1.1/mysql/bin:/c/ProgramData/ComposerSetup/bin:/cmd:/c/Program Files/Microsoft VS Code/bin:${HOME}/AppData/Roaming/Composer/vendor/bin:/usr/bin/vendor_perl:/usr/bin/core_perl"
+cleanPath="/usr/local/bin:/usr/bin:/bin:/mingw64/bin:/c/Windows/system32:/c/Windows:/c/Windows/System32/WindowsPowerShell/v1.0"
+cleanPath="${cleanPath}:${HOME}/AppData/Roaming/nvm:/c/Program Files/nodejs:/cmd:/c/Program Files/Microsoft VS Code/bin";
+cleanPath="${cleanPath}:/usr/bin/vendor_perl:/usr/bin/core_perl"
 
 PATH="$HOME/bin:$HOME/.bin:$HOME/.local/bin:${DOTFILES_SHELL_PATH}/windows/bin:${DOTFILES_SHELL_PATH}/bin:${cleanPath}"
 
