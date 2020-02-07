3b0a915d8a680bcf8d090f2efb582a93312d288c
Carlos Gomes <carlos.algms@gmail.com>
2020-02-07 11:29:13 +0100

    Disable ubuntu default colors for zsh

--- shell/linux/bash/bash_colors.sh
+++ shell/linux/bash/bash_colors.sh
@@ -1,3 +1,6 @@
+if [[ "$SHELL" =~ zsh$ ]]; then
+    return 0
+fi
 
 # Ubuntu default prompt colors
 PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
