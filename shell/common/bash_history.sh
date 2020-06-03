47243f16e241a8271d59edcb9c087fc17ba01cc0
Carlos Gomes <carlos.algms@gmail.com>
2020-06-03 09:35:59 -0300

    Enhanced cmd history for ZSH

--- shell/common/bash_history.sh
+++ shell/common/bash_history.sh
@@ -1,14 +1,22 @@
-if [ ! -z "$IS_MAC" ]; then
-    return 0
-fi
-
-if [ -x "$(command -v shopt)" ]; then
+# TODO need to check if it is properly working on Win and Linux
+if [ -x "$(command -v shopt)" ] || [ ! -z "$(command -v shopt)" ] ; then
     # append to history
     shopt -s histappend
     # Combine multiline commands into one in history
     shopt -s cmdhist
     # Do not autocomplete when accidentally pressing Tab on an empty line.
     shopt -s no_empty_cmd_completion;
+fi
+
+if [ -x "$(command -v setopt)" ] || [ ! -z "$(command -v setopt)" ] ; then
+    setopt EXTENDED_HISTORY
+    setopt HIST_EXPIRE_DUPS_FIRST
+    setopt HIST_IGNORE_DUPS
+    setopt HIST_IGNORE_ALL_DUPS
+    setopt HIST_IGNORE_SPACE
+    setopt HIST_FIND_NO_DUPS
+    setopt HIST_SAVE_NO_DUPS
+    setopt HIST_REDUCE_BLANKS
 fi
 
 # Ignore duplicates, ls without options and builtin commands
