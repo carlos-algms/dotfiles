f648e8ef0827fea6af900aa71c18506f67b68699
Carlos Gomes <carlos.algms@gmail.com>
2021-10-26 13:53:50 +0200

    Extract non-default windows path to personal .dotfilesrc config

--- shell/windows/bash/01_path_windows.sh
+++ shell/windows/bash/01_path_windows.sh
@@ -12,12 +12,8 @@ cleanPath="${cleanPath}:/usr/bin"
 cleanPath="${cleanPath}:/bin"
 cleanPath="${cleanPath}:/mingw64/bin"
 cleanPath="${cleanPath}:/cmd"
-cleanPath="${cleanPath}:/c/Program Files/nodejs"
 cleanPath="${cleanPath}:/usr/bin/vendor_perl"
 cleanPath="${cleanPath}:/usr/bin/core_perl"
-
-cleanPath="${cleanPath}:$HOME/AppData/Local/Programs/Microsoft VS Code/bin";
-cleanPath="${cleanPath}:/c/Program Files (x86)/Yarn/bin"
 
 cleanPath="${cleanPath}:/c/Windows/system32"
 cleanPath="${cleanPath}:/c/Windows/System32/wbem"
@@ -25,4 +21,3 @@ cleanPath="${cleanPath}:/c/Windows"
 cleanPath="${cleanPath}:/c/Windows/System32/WindowsPowerShell/v1.0"
 
 export PATH="${cleanPath}"
-export NODE_PATH="/c/Program Files/nodejs/node_modules"
