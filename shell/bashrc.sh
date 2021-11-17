0f6ec1406d076e8de8b43768d06a8f3c1a7d2ffa
Carlos Gomes <carlos.algms@gmail.com>
2021-11-17 16:33:12 +0100

    Commenting paths and source files that might not be there

--- shell/bashrc.sh
+++ shell/bashrc.sh
@@ -38,15 +38,15 @@
         e_error "No OS identified to source files"
     fi
 
-    # Load user specific aliases
-    if [ -f ~/.bash_aliases ]; then
-        source ~/.bash_aliases
-    fi
+    # # Load user specific aliases
+    # if [ -f ~/.bash_aliases ]; then
+    #     source ~/.bash_aliases
+    # fi
 
-    # Load user specific functions
-    if [ -f ~/.bash_functions ]; then
-        source ~/.bash_functions
-    fi
+    # # Load user specific functions
+    # if [ -f ~/.bash_functions ]; then
+    #     source ~/.bash_functions
+    # fi
 
     # Load custom configuration
     if [ -f ~/.dotfilesrc ]; then
