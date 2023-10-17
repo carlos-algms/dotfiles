1dab7e4bc4be8f04514d0b299d3ad72a5790e147
Carlos Gomes <carlos.algms@gmail.com>
2023-10-17 17:00:50 +0200

    Add pnpm aliases as it isnt included with oh-my-zsh

---
+++ shell/common/pnpm.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+alias p='pnpm'
+alias pa='pnpm add'
+alias pad='pnpm add --save-dev'
+alias prm='pnpm remove'
+alias pga='pnpm add --global'
+alias pgls='pnpm list --global'
+alias pgrm='pnpm remove --global'
+alias pin='pnpm install'
+alias pex='pnpm exec'
+alias pdx='pnpm dlx'
+alias pr='pnpm run'
