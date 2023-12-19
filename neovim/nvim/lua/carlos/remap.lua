77d6220e07ccd26276a728a5e5f92dadac44695b
Carlos Gomes <carlos.algms@gmail.com>
2023-12-19 21:02:15 +0100

    Add copy to clipboard remap <leader>y and <leader>Y

--- neovim/nvim/lua/carlos/remap.lua
+++ neovim/nvim/lua/carlos/remap.lua
@@ -10,11 +10,10 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
-
 -- Don't lose yank when pasting over a selected content (AKA: replace)
-vim.keymap.set("x", "p", [["_dP]])
+-- vim.keymap.set("x", "p", [["_dP]])
 -- Don't lose yank when deleting selected content
-vim.keymap.set("v", "d", [["_d]])
+-- vim.keymap.set("v", "d", [["_d]])
 
 -- Keeps the cursor on the center when moving to next/prev search results
 vim.keymap.set("n", "n", "nzzzv")
@@ -23,6 +22,9 @@ vim.keymap.set("n", "N", "Nzzzv")
 -- replace the selected content without losing the Yanked register
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
+-- copy to system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set("n", "Q", "<nop>")
 
