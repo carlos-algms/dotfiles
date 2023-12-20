0035c50b00efa6812a95bd3af5786180359548bf
Carlos Gomes <carlos.algms@gmail.com>
2023-12-20 12:13:47 +0100

    Make split below and right default

--- neovim/nvim/lua/carlos/set.lua
+++ neovim/nvim/lua/carlos/set.lua
@@ -1,4 +1,3 @@
-
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
@@ -6,7 +5,7 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-
+vim.opt.autoindent = true
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
@@ -17,15 +16,18 @@ vim.opt.backup = false
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+vim.opt.background = "dark"
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
+vim.opt.colorcolumn = "80"
+vim.opt.scrolloff = 99
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
-
+vim.opt.splitright = true
+vim.opt.splitbelow = true
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
-
-
+vim.scriptencoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+vim.opt.encoding = "utf-8"
