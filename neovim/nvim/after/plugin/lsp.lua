6b44db3807877bfe471861c9b7a748052606ca81
Carlos Gomes <carlos.algms@gmail.com>
2023-12-20 12:10:31 +0100

    Fixed Lua not auto accepting snippets, it was working in other files

--- neovim/nvim/after/plugin/lsp.lua
+++ neovim/nvim/after/plugin/lsp.lua
@@ -90,9 +90,15 @@ masonLspconfig.setup_handlers({
 })
 
 local cmp = require("cmp")
+local luasnip = require("luasnip")
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 cmp.setup({
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
     sources = {
         { name = "path" },
         { name = "nvim_lsp" },
