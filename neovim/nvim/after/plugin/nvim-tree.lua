bc1cfeceb98768c13995a992083912e719d901ba
Carlos Gomes <carlos.algms@gmail.com>
2023-12-21 19:17:53 +0100

    Add Ctrl + Shift + N to toggle the tree

--- neovim/nvim/after/plugin/nvim-tree.lua
+++ neovim/nvim/after/plugin/nvim-tree.lua
@@ -5,30 +5,30 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
-  on_attach = function(bufnr)
-    local api = require "nvim-tree.api"
+    sort_by = "case_sensitive",
+    view = {
+        width = 30,
+    },
+    renderer = {
+        group_empty = true,
+    },
+    filters = {
+        dotfiles = true,
+    },
+    on_attach = function(bufnr)
+        local api = require("nvim-tree.api")
 
-    local function opts(desc)
-        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-    end
+        local function opts(desc)
+            return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
 
-    -- default mappings
-    api.config.mappings.default_on_attach(bufnr)
+        -- default mappings
+        api.config.mappings.default_on_attach(bufnr)
 
-    -- vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>')
-    vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
-  end,
+        -- vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>')
+        vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
+    end,
 })
 
-vim.keymap.set('n', '<C-n>', ':NvimTreeFindFile<CR>')
-
+vim.keymap.set("n", "<C-n>", ":NvimTreeFindFile<CR>")
+vim.keymap.set("n", "<SC-n>", ":NvimTreeToggle<CR>")
