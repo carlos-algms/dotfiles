6cc5b31afcc419e9b22f3e852f3d90c0ea757222
Carlos Gomes <carlos.algms@gmail.com>
2023-12-23 12:29:52 +0100

    Set undo tree cache to a permanent folder

--- neovim/nvim/lua/plugins/undotree.lua
+++ neovim/nvim/lua/plugins/undotree.lua
@@ -2,5 +2,9 @@ return {
     "mbbill/undotree",
     config = function()
         vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+
+        -- set it to a permananet folder so I can undo across sessions
+        vim.opt.undodir = os.getenv("HOME") .. "/.cache/undodir"
+        vim.g.undotree_SetFocusWhenToggle = 1
     end,
 }
