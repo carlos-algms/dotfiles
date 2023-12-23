3311b8137f4eccb2eb654919e6d9b1c2efaadb7d
Carlos Gomes <carlos.algms@gmail.com>
2023-12-23 12:08:34 +0100

    Fix formatter after cleaning Packer cache and old installs

--- neovim/nvim/lua/plugins/formatter.lua
+++ neovim/nvim/lua/plugins/formatter.lua
@@ -4,52 +4,52 @@ return {
     -- opts = function()
     --     return require "custom.configs.formatter"
     -- end
-    opts = {
-        -- All formatter configurations are opt-in
-        filetype = {
-            javascript = {
-                require("formatter.filetypes.javascript").prettier,
-            },
-            javascriptreact = {
-                require("formatter.filetypes.javascriptreact").prettier,
-            },
-            typescript = {
-                require("formatter.filetypes.typescript").prettier,
-            },
-            typescriptreact = {
-                require("formatter.filetypes.typescriptreact").prettier,
-            },
-            json = {
-                require("formatter.filetypes.json").prettier,
-            },
-            html = {
-                require("formatter.filetypes.html").prettier,
-            },
-            css = {
-                require("formatter.filetypes.css").prettier,
-            },
-            scss = {
-                require("formatter.filetypes.css").prettier,
-            },
-            markdown = {
-                require("formatter.filetypes.markdown").prettier,
-            },
-            lua = {
-                require("formatter.filetypes.lua").stylua,
-            },
-            yaml = {
-                require("formatter.filetypes.yaml").prettier,
-            },
-            ["*"] = {
-                require("formatter.filetypes.any").remove_trailing_whitespace,
-            },
-        },
-    },
-    config = function(_, opts)
+    config = function()
         -- enable auto-formatting on save
         vim.api.nvim_create_autocmd({ "BufWritePost" }, {
             command = "FormatWriteLock",
         })
-        require("formatter").setup(opts)
+
+        require("formatter").setup({
+            -- All formatter configurations are opt-in
+            filetype = {
+                javascript = {
+                    require("formatter.filetypes.javascript").prettier,
+                },
+                javascriptreact = {
+                    require("formatter.filetypes.javascriptreact").prettier,
+                },
+                typescript = {
+                    require("formatter.filetypes.typescript").prettier,
+                },
+                typescriptreact = {
+                    require("formatter.filetypes.typescriptreact").prettier,
+                },
+                json = {
+                    require("formatter.filetypes.json").prettier,
+                },
+                html = {
+                    require("formatter.filetypes.html").prettier,
+                },
+                css = {
+                    require("formatter.filetypes.css").prettier,
+                },
+                scss = {
+                    require("formatter.filetypes.css").prettier,
+                },
+                markdown = {
+                    require("formatter.filetypes.markdown").prettier,
+                },
+                lua = {
+                    require("formatter.filetypes.lua").stylua,
+                },
+                yaml = {
+                    require("formatter.filetypes.yaml").prettier,
+                },
+                ["*"] = {
+                    require("formatter.filetypes.any").remove_trailing_whitespace,
+                },
+            },
+        })
     end,
 }
