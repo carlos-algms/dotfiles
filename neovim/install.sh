007d0e3097bd8fef41f4f2e1ac9daaf526475fb4
Carlos Gomes <carlos.algms@gmail.com>
2024-01-02 21:40:42 +0100

    Fix neovim install script

--- neovim/install.sh
+++ neovim/install.sh
@@ -1,4 +1,27 @@
 #!/usr/bin/env bash
 
-# ripgrep is required for grep_string
-brew install neovim ripgrep
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source "${SCRIPT_DIR}/common/00_os.sh"
+source "${SCRIPT_DIR}/common/01_logging.sh"
+
+if [ -z "$(command -v nvim)" ]; then
+    if [[ ! -z "$IS_MAC" ]]; then
+        # ripgrep is required for grep_string, telescope, etc..
+        brew install neovim ripgrep
+    elif [[ ! -z "$IS_LINUX" ]]; then
+        sudo apt-get install -y neovim ripgrep
+    else
+        log_error "OS not supported"
+        exit 1
+    fi
+fi
+
+target="${HOME}/.config/nvim"
+
+if [ -d "$target" ] || [ -L "$target" ]; then
+    TODAY="`date`"
+    mv "$target" "$target-${TODAY}"
+fi
+
+ln -snf "${SCRIPT_DIR}/nvim" "$target"
