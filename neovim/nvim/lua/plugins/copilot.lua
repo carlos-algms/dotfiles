7a9ca38026838064be4e7bd1c6354feb53dd40c7
Carlos Gomes <carlos.algms@gmail.com>
2024-01-08 13:35:08 +0100

    Enable copilot for yaml and markdown

--- neovim/nvim/lua/plugins/copilot.lua
+++ neovim/nvim/lua/plugins/copilot.lua
@@ -1,3 +1,10 @@
 return {
     "github/copilot.vim",
+    config = function()
+        vim.g.copilot_filetypes = {
+            yml = true,
+            yaml = true,
+            markdown = true,
+        }
+    end,
 }
