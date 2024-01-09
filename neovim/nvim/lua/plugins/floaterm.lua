b38e622d3968b12d25fc1d1e4a8cf8d8d2ab6e09
Carlos Gomes <carlos.algms@gmail.com>
2024-01-09 01:21:40 +0100

    Add floaterm as the terminal plugin

---
+++ neovim/nvim/lua/plugins/floaterm.lua
@@ -0,0 +1,10 @@
+return {
+    "voldikss/vim-floaterm",
+    init = function()
+        vim.g.floaterm_keymap_toggle = "<c-`>"
+    end,
+    -- config = function()
+    --     vim.keymap.set("n", "<leader>t", "<cmd>FloatermToggle<cr>")
+    --     -- vim.keymap.set("n", "<f12>", "<cmd>FloatermToggle<cr>")
+    -- end,
+}
