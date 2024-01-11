2896e106e94ee15e900637b256146768f77a4198
Carlos Gomes <carlos.algms@gmail.com>
2024-01-11 22:46:37 +0100

    Make fugitive commit in vertical split to the right

--- neovim/nvim/lua/plugins/fugitive.lua
+++ neovim/nvim/lua/plugins/fugitive.lua
@@ -1,3 +1,22 @@
 return {
     "tpope/vim-fugitive",
+    config = function()
+        vim.api.nvim_create_autocmd("FileType", {
+            desc = "Fugitive overrides",
+            pattern = { "fugitive" },
+            group = vim.api.nvim_create_augroup(
+                "UserFugitive",
+                { clear = true }
+            ),
+            callback = function(ev)
+                vim.api.nvim_buf_set_keymap(
+                    ev.buf,
+                    "n",
+                    "cc",
+                    ":<C-U> vertical Git commit<CR>",
+                    {}
+                )
+            end,
+        })
+    end,
 }
