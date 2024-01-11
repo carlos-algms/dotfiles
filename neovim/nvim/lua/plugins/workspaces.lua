7ade97ff23537b439f8425146bb36508a3d1f20f
Carlos Gomes <carlos.algms@gmail.com>
2024-01-11 22:29:22 +0100

    Add telescope extension for workspaces

--- neovim/nvim/lua/plugins/workspaces.lua
+++ neovim/nvim/lua/plugins/workspaces.lua
@@ -1,6 +1,10 @@
 return {
     "natecraddock/workspaces.nvim",
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+    },
     config = function()
         require("workspaces").setup()
+        require("telescope").load_extension("workspaces")
     end,
 }
