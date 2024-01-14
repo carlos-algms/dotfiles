22e5fe1ab83f059d9ac3918a44c916af1162dd0b
Carlos Gomes <carlos.algms@gmail.com>
2024-01-14 20:10:21 +0100

    Make fugitive behave like a sidebar

--- neovim/nvim/lua/plugins/fugitive.lua
+++ neovim/nvim/lua/plugins/fugitive.lua
@@ -4,14 +4,18 @@ return {
         "nvim-telescope/telescope.nvim",
     },
     config = function()
+        local fugitiveWinId = nil
         local telescopeBuiltin = require("telescope.builtin")
 
-        vim.keymap.set(
-            "n",
-            "<leader>gs",
-            ":Git<CR>:17wincmd _<CR>",
-            { desc = "Show [g]it [s]tatus" , silent = true}
-        )
+        vim.keymap.set("n", "<leader>gs", function()
+            vim.cmd("Git")
+            fugitiveWinId = vim.api.nvim_get_current_win()
+            vim.cmd("wincmd o ")
+            vim.cmd("vnew")
+            vim.cmd("wincmd 1w")
+            vim.api.nvim_win_set_width(fugitiveWinId, 50)
+            vim.cmd("norm 0)")
+        end, { desc = "Show [g]it [s]tatus", silent = true })
 
         vim.keymap.set(
             "n",
@@ -39,10 +43,42 @@ return {
                     ev.buf,
                     "n",
                     "cc",
-                    ":<C-U> tab Git commit<CR>",
+                    ":<C-U> wincmd o<CR> :vertical Git commit<CR>",
                     {}
                 )
             end,
+        })
+
+        local group = vim.api.nvim_create_augroup(
+            "UserFugitiveOverrides",
+            { clear = true }
+        )
+
+        vim.api.nvim_create_autocmd("BufEnter", {
+            pattern = "COMMIT_EDITMSG",
+            callback = function()
+                vim.defer_fn(function()
+                    if fugitiveWinId then
+                        vim.api.nvim_win_set_width(fugitiveWinId, 50)
+                    end
+                end, 50)
+            end,
+            group = group,
+        })
+
+        vim.api.nvim_create_autocmd("BufLeave", {
+            pattern = "COMMIT_EDITMSG",
+            callback = function()
+                vim.cmd("vnew")
+
+                if fugitiveWinId then
+                    vim.defer_fn(function()
+                        vim.api.nvim_set_current_win(fugitiveWinId)
+                        vim.api.nvim_win_set_width(fugitiveWinId, 50)
+                    end, 50)
+                end
+            end,
+            group = group,
         })
     end,
 }
