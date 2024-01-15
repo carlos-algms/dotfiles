20944ac7832588ee48aa3a08e75725d8752d34eb
Carlos Gomes <carlos.algms@gmail.com>
2024-01-15 07:09:26 +0100

    Add more modes to the Alt + q mapping

--- neovim/nvim/lua/remap.lua
+++ neovim/nvim/lua/remap.lua
@@ -67,18 +67,19 @@ vim.keymap.set({ "n", "v" }, "Y", [[yy]], { desc = "Yank entire line" })
 vim.keymap.set("n", "Q", "<nop>")
 
 -- replace all occurences of the word under the cursor
-vim.keymap.set(
-    "n",
-    "<leader>s",
-    [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
-    { desc = "Replace all occurences of the word under the cursor" }
-)
+-- vim.keymap.set(
+--     "n",
+--     "<leader>s",
+--     [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+--     { desc = "Replace all occurences of the word under the cursor" }
+-- )
 
 -- window management
 vim.keymap.set("n", "<leader>w", "<C-w>", { desc = "Ctrl + w replacement" })
 
-
-vim.keymap.set({"n", "v", "i", "x"}, "œ", "<Esc>", { desc = "Alt + q - Same as <Esc>" })
-
-
-
+vim.keymap.set(
+    { "n", "v", "i", "x" },
+    "œ",
+    "<Esc>",
+    { desc = "Alt + q - Same as <Esc>" }
+)
