729b148fb3ec5723b2814ff7fde285c853533597
Carlos Gomes <carlos.algms@gmail.com>
2024-01-17 00:44:02 +0100

    Integrate illuminate with Darcula theme

--- neovim/nvim/lua/plugins/illuminate.lua
+++ neovim/nvim/lua/plugins/illuminate.lua
@@ -1,16 +1,35 @@
 return {
     "RRethy/vim-illuminate",
+    enabled = true,
     config = function()
         local illuminate = require("illuminate")
+
+        illuminate.configure({
+            providers = {
+                "lsp",
+                "treesitter",
+                -- "regex",
+            },
+        })
+
+        vim.cmd([[ hi! clear IlluminatedWordText ]])
+        vim.cmd([[ hi! clear IlluminatedWordWrite ]])
+        vim.cmd([[ hi! clear IlluminatedWordRead ]])
+
+        vim.cmd([[ hi! IlluminatedWordText guibg=#393b4e ]])
+        vim.cmd([[ hi! link IlluminatedWordRead IlluminatedWordText ]])
+        vim.cmd([[ hi! link IlluminatedWordWrite IlluminatedWordText ]])
+
         vim.keymap.set(
             "n",
-            "]s",
+            "]r",
             illuminate.goto_next_reference,
             { desc = "Go to next reference" }
         )
+
         vim.keymap.set(
             "n",
-            "[s",
+            "[r",
             illuminate.goto_prev_reference,
             { desc = "Go to previous reference" }
         )
