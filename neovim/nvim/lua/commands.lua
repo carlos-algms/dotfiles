a757f98cb8738ad7e08cede94216bfa487266d16
Carlos Gomes <carlos.algms@gmail.com>
2024-01-17 00:55:35 +0100

    Create a Float buffer to display the output of commands

--- neovim/nvim/lua/commands.lua
+++ neovim/nvim/lua/commands.lua
@@ -5,3 +5,37 @@ vim.api.nvim_create_user_command("Redir", function(ctx)
     vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
     vim.opt_local.modified = false
 end, { nargs = "+", complete = "command" })
+
+vim.api.nvim_create_user_command("Float", function(ctx)
+    local width = 130
+    local height = 25
+
+    -- Create the scratch buffer displayed in the floating window
+    local buf = vim.api.nvim_create_buf(false, true)
+
+    -- close the buffer with q
+    vim.keymap.set({ "n", "v" }, "q", ":q<CR>", { buffer = buf })
+
+    -- Set the buffer's contents to the output of the command
+    local ouput = vim.api.nvim_exec(ctx.args, true)
+    local lines = vim.split(ouput, "\n", { plain = true })
+    vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+
+    -- Get the current UI
+    local uis = vim.api.nvim_list_uis()
+
+    local ui = uis[0] or uis[1]
+
+    -- Create the floating window
+    local opts = {
+        relative = "editor",
+        width = width,
+        height = height,
+        col = (ui.width / 2) - (width / 2),
+        row = (ui.height / 2) - (height / 2),
+        anchor = "NW",
+        style = "minimal",
+    }
+
+    local win = vim.api.nvim_open_win(buf, true, opts)
+end, { nargs = "+", complete = "command" })
