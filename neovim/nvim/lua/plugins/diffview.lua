7cf2c261d647aab547abdc27c930dcd000cd4646
Carlos Gomes <carlos.algms@gmail.com>
2024-01-17 20:03:12 +0100

    Use Diffview as default and Fugitive for commit only

--- neovim/nvim/lua/plugins/diffview.lua
+++ neovim/nvim/lua/plugins/diffview.lua
@@ -3,25 +3,65 @@
 -- 2. does not offer a commit command
 return {
     "sindrets/diffview.nvim",
-    enabled = false,
+    enabled = true,
     config = function()
         local actions = require("diffview.actions")
 
+        vim.keymap.set(
+            "n",
+            "<leader>gs",
+            ":DiffviewOpen<cr>",
+            { silent = true }
+        )
+
+        local quitDiffViewKeymap = {
+            "n",
+            "gq",
+            ":tabclose<cr>",
+            { desc = "Close the Diffview" },
+        }
+
+        local commitKeyMap = {
+            "n",
+            "cc",
+            ":tab G commit<cr>",
+            { desc = "Commit staged files" },
+        }
+
         require("diffview").setup({
+            default_args = {
+                DiffviewOpen = { "--imply-local" },
+            },
             file_panel = {
                 listing_style = "list",
             },
             keymaps = {
                 disable_defaults = false, -- Disable the default keymaps
                 file_panel = {
+                    quitDiffViewKeymap,
+                    commitKeyMap,
+
                     {
                         "n",
                         "<cr>",
                         function()
                             actions.select_entry()
-                            vim.cmd("wincmd 3w") -- jump to the diff window
+                            vim.cmd("wincmd 2w")
+                            vim.cmd("norm gg")
+                            vim.cmd("wincmd 3w")
+                            vim.cmd("norm gg]c")
                         end,
                         { desc = "Open the diff for the selected entry" },
+                    },
+                },
+                view = {
+                    quitDiffViewKeymap,
+                    commitKeyMap,
+                    {
+                        "n",
+                        "s",
+                        actions.toggle_stage_entry,
+                        { desc = "Stage / unstage the selected entry" },
                     },
                 },
             },
--- neovim/nvim/lua/plugins/fugitive.lua
+++ neovim/nvim/lua/plugins/fugitive.lua
@@ -4,7 +4,6 @@ return {
         "nvim-telescope/telescope.nvim",
     },
     config = function()
-        local fugitiveWinId = nil
         local telescopeBuiltin = require("telescope.builtin")
 
         vim.keymap.set(
@@ -13,16 +12,6 @@ return {
             ":Git push<CR>",
             { desc = "[G]it [P]ush" }
         )
-
-        vim.keymap.set("n", "<leader>gs", function()
-            vim.cmd("Git")
-            fugitiveWinId = vim.api.nvim_get_current_win()
-            vim.cmd("wincmd o ")
-            vim.cmd("vnew")
-            vim.cmd("wincmd 1w")
-            vim.api.nvim_win_set_width(fugitiveWinId, 50)
-            vim.cmd("norm 0)")
-        end, { desc = "Show [g]it [s]tatus", silent = true })
 
         vim.keymap.set(
             "n",
@@ -38,54 +27,67 @@ return {
             { desc = "Show [g]it [d]iff for current file" }
         )
 
-        vim.api.nvim_create_autocmd("FileType", {
-            desc = "Fugitive overrides",
-            pattern = { "fugitive" },
-            group = vim.api.nvim_create_augroup(
-                "UserFugitive",
-                { clear = true }
-            ),
-            callback = function(ev)
-                vim.api.nvim_buf_set_keymap(
-                    ev.buf,
-                    "n",
-                    "cc",
-                    ":<C-U> wincmd o<CR> :vertical Git commit<CR>",
-                    {}
-                )
-            end,
-        })
+        -- Disabling to use Diffview as default
+        -- local fugitiveWinId = nil
 
-        local group = vim.api.nvim_create_augroup(
-            "UserFugitiveOverrides",
-            { clear = true }
-        )
+        -- vim.keymap.set("n", "<leader>gs", function()
+        --     vim.cmd("Git")
+        --     fugitiveWinId = vim.api.nvim_get_current_win()
+        --     vim.cmd("wincmd o ")
+        --     vim.cmd("vnew")
+        --     vim.cmd("wincmd 1w")
+        --     vim.api.nvim_win_set_width(fugitiveWinId, 50)
+        --     vim.cmd("norm 0)")
+        -- end, { desc = "Show [g]it [s]tatus", silent = true })
 
-        vim.api.nvim_create_autocmd("BufEnter", {
-            pattern = "COMMIT_EDITMSG",
-            callback = function()
-                vim.defer_fn(function()
-                    if fugitiveWinId then
-                        vim.api.nvim_win_set_width(fugitiveWinId, 50)
-                    end
-                end, 50)
-            end,
-            group = group,
-        })
+        -- vim.api.nvim_create_autocmd("FileType", {
+        --     desc = "Fugitive overrides",
+        --     pattern = { "fugitive" },
+        --     group = vim.api.nvim_create_augroup(
+        --         "UserFugitive",
+        --         { clear = true }
+        --     ),
+        --     callback = function(ev)
+        --         vim.api.nvim_buf_set_keymap(
+        --             ev.buf,
+        --             "n",
+        --             "cc",
+        --             ":<C-U> wincmd o<CR> :vertical Git commit<CR>",
+        --             {}
+        --         )
+        --     end,
+        -- })
 
-        vim.api.nvim_create_autocmd("BufLeave", {
-            pattern = "COMMIT_EDITMSG",
-            callback = function()
-                vim.cmd("vnew")
+        -- local group = vim.api.nvim_create_augroup(
+        --     "UserFugitiveOverrides",
+        --     { clear = true }
+        -- )
 
-                if fugitiveWinId then
-                    vim.defer_fn(function()
-                        vim.api.nvim_set_current_win(fugitiveWinId)
-                        vim.api.nvim_win_set_width(fugitiveWinId, 50)
-                    end, 50)
-                end
-            end,
-            group = group,
-        })
+        -- vim.api.nvim_create_autocmd("BufEnter", {
+        --     pattern = "COMMIT_EDITMSG",
+        --     callback = function()
+        --         vim.defer_fn(function()
+        --             if fugitiveWinId then
+        --                 vim.api.nvim_win_set_width(fugitiveWinId, 50)
+        --             end
+        --         end, 50)
+        --     end,
+        --     group = group,
+        -- })
+
+        -- vim.api.nvim_create_autocmd("BufLeave", {
+        --     pattern = "COMMIT_EDITMSG",
+        --     callback = function()
+        --         vim.cmd("vnew")
+
+        --         if fugitiveWinId then
+        --             vim.defer_fn(function()
+        --                 vim.api.nvim_set_current_win(fugitiveWinId)
+        --                 vim.api.nvim_win_set_width(fugitiveWinId, 50)
+        --             end, 50)
+        --         end
+        --     end,
+        --     group = group,
+        -- })
     end,
 }
