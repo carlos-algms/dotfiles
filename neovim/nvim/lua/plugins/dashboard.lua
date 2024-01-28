6837486b7b9503b077db71a8ace2ab1b3893eccb
Carlos Gomes <carlos.algms@gmail.com>
2024-01-28 16:25:30 +0100

    Enable Dashboard plugin

---
+++ neovim/nvim/lua/plugins/dashboard.lua
@@ -0,0 +1,51 @@
+return {
+    "nvimdev/dashboard-nvim",
+    dir = "~/projects/dashboard-nvim",
+    event = "VimEnter",
+    dependencies = {
+        { "nvim-tree/nvim-web-devicons" },
+    },
+    config = function()
+        require("dashboard").setup({
+            theme = "hyper",
+            shortcut_type = "number",
+            config = {
+                week_header = {
+                    enable = true,
+                },
+                shortcut = {
+                    {
+                        desc = "󰊳 Update",
+                        group = "Function",
+                        action = "Lazy update",
+                        key = "U",
+                    },
+                    {
+                        desc = "󰊵 New file",
+                        group = "Type",
+                        action = "enew",
+                        key = "n",
+                    },
+                    {
+                        desc = "󰊻 Quit",
+                        group = "Keyword",
+                        action = "q",
+                        key = "q",
+                    },
+                },
+                footer = {
+                    "",
+                },
+                project = {
+                    enable = false,
+                },
+                mru = {
+                    limit = 9, -- limit to 9, other 1 becomes too slow, as it will wait to see I press 0
+                    cwd_only = true,
+                },
+            },
+        })
+
+        vim.cmd.hi("link DashboardHeader Type")
+    end,
+}
