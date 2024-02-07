058a71680328d138b2d11d133ebaa8880faafc3f
Carlos Gomes <carlos.algms@gmail.com>
2024-02-07 20:40:24 +0100

    Add plugin to copy link to Github

--- neovim/nvim/lua/plugins/gitsigns.lua
+++ neovim/nvim/lua/plugins/gitsigns.lua
@@ -1,36 +1,47 @@
 return {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-        require("gitsigns").setup({
-            on_attach = function(bufnr)
-                local gs = package.loaded.gitsigns
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup({
+                on_attach = function(bufnr)
+                    local gs = package.loaded.gitsigns
 
-                local function map(mode, l, r, opts)
-                    opts = opts or {}
-                    opts.buffer = bufnr
-                    vim.keymap.set(mode, l, r, opts)
-                end
-
-                map("n", "]c", function()
-                    if vim.wo.diff then
-                        return "]c"
+                    local function map(mode, l, r, opts)
+                        opts = opts or {}
+                        opts.buffer = bufnr
+                        vim.keymap.set(mode, l, r, opts)
                     end
-                    vim.schedule(function()
-                        gs.next_hunk()
-                    end)
-                    return "<Ignore>"
-                end, { desc = "next change hunk", expr = true })
 
-                map("n", "[c", function()
-                    if vim.wo.diff then
-                        return "[c"
-                    end
-                    vim.schedule(function()
-                        gs.prev_hunk()
-                    end)
-                    return "<Ignore>"
-                end, { desc = "prev change hunk", expr = true })
-            end,
-        })
-    end,
+                    map("n", "]c", function()
+                        if vim.wo.diff then
+                            return "]c"
+                        end
+                        vim.schedule(function()
+                            gs.next_hunk()
+                        end)
+                        return "<Ignore>"
+                    end, {
+                        desc = "next change hunk",
+                        expr = true,
+                    })
+
+                    map("n", "[c", function()
+                        if vim.wo.diff then
+                            return "[c"
+                        end
+                        vim.schedule(function()
+                            gs.prev_hunk()
+                        end)
+                        return "<Ignore>"
+                    end, {
+                        desc = "prev change hunk",
+                        expr = true,
+                    })
+                end,
+            })
+        end,
+    },
+    {
+        "knsh14/vim-github-link",
+    },
 }
