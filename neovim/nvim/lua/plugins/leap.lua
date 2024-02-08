ad70d67960930b7e53e7003b787dadd4e0b51e1d
Carlos Gomes <carlos.algms@gmail.com>
2024-02-08 21:51:12 +0100

    Re-enable Leap with settings copied from LazyVim

--- neovim/nvim/lua/plugins/leap.lua
+++ neovim/nvim/lua/plugins/leap.lua
@@ -1,8 +1,23 @@
 return {
     "ggandor/leap.nvim",
-    enabled = false,
-    config = function()
+    enabled = true,
+    -- config = function()
+    --     local leap = require("leap")
+    --     leap.create_default_mappings()
+    -- end,
+    -- copy from https://www.lazyvim.org/extras/editor/leap
+    keys = {
+        { "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+        { "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
+        { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+    },
+    config = function(_, opts)
         local leap = require("leap")
-        leap.create_default_mappings()
+        for k, v in pairs(opts) do
+            leap.opts[k] = v
+        end
+        leap.add_default_mappings(true)
+        vim.keymap.del({ "x", "o" }, "x")
+        vim.keymap.del({ "x", "o" }, "X")
     end,
 }
