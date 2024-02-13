f023a5f6935271b0a5b58c3d39de72dd78291338
Carlos Gomes <carlos.algms@gmail.com>
2024-02-13 14:01:00 +0100

    Normalise imports to be white

--- neovim/nvim/after/queries/typescript/highlights.scm
+++ neovim/nvim/after/queries/typescript/highlights.scm
@@ -10,3 +10,25 @@
    (string) @type.literal.string (#set! "priority" 150)
    )
 )
+
+(import_statement
+  (import_clause
+    (named_imports
+      (import_specifier
+        name: (identifier) @import.identifier
+        )
+      )
+    )
+  )
+
+(lexical_declaration
+  (variable_declarator
+    value: (new_expression
+             constructor: (identifier) @new.identifier (#set! "priority" 150)
+             )
+    )
+  )
+
+(namespace_import
+  (identifier) @import.identifier
+  )
