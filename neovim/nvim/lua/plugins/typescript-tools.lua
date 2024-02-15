caa01a24ee3e8599b47638f2bbe5cdb04dc252f5
Carlos Gomes <carlos.algms@gmail.com>
2024-02-15 16:36:23 +0100

    Add TSC command to check the entire project

--- neovim/nvim/lua/plugins/typescript-tools.lua
+++ neovim/nvim/lua/plugins/typescript-tools.lua
@@ -1,56 +1,65 @@
 return {
-    "pmizio/typescript-tools.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    opts = {
-        expose_as_code_action = "all",
-        complete_function_calls = { "all" },
-        jsx_close_tag = {
-            enable = true,
-            filetypes = { "javascriptreact", "typescriptreact" },
+    {
+        "pmizio/typescript-tools.nvim",
+        dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+        opts = {
+            expose_as_code_action = "all",
+            complete_function_calls = { "all" },
+            jsx_close_tag = {
+                enable = true,
+                filetypes = { "javascriptreact", "typescriptreact" },
+            },
+            on_attach = function(config, bufNr)
+                vim.keymap.set(
+                    { "n", "v" },
+                    "<space>io",
+                    ":TSToolsOrganizeImports<CR>",
+                    { desc = "Imports Organize", silent = true, buffer = bufNr }
+                )
+
+                vim.keymap.set(
+                    { "n", "v" },
+                    "<space>is",
+                    ":TSToolsSortImports<CR>",
+                    { desc = "Imports Sort", silent = true, buffer = bufNr }
+                )
+
+                vim.keymap.set(
+                    { "n", "v" },
+                    "<space>ir",
+                    ":TSToolsRemoveUnusedImports<CR>",
+                    {
+                        desc = "Imports remove unused",
+                        silent = true,
+                        buffer = bufNr,
+                    }
+                )
+
+                vim.keymap.set(
+                    { "n", "v" },
+                    "<space>ia",
+                    ":TSToolsAddMissingImports<CR>",
+                    {
+                        desc = "Imports Add All missing",
+                        silent = true,
+                        buffer = bufNr,
+                    }
+                )
+
+                vim.keymap.set(
+                    { "n", "v" },
+                    "<space>rf",
+                    ":TSToolsRenameFile<CR>",
+                    { desc = "Rename File", silent = true, buffer = bufNr }
+                )
+            end,
         },
-        on_attach = function(config, bufNr)
-            vim.keymap.set(
-                { "n", "v" },
-                "<space>io",
-                ":TSToolsOrganizeImports<CR>",
-                { desc = "Imports Organize", silent = true, buffer = bufNr }
-            )
+    },
 
-            vim.keymap.set(
-                { "n", "v" },
-                "<space>is",
-                ":TSToolsSortImports<CR>",
-                { desc = "Imports Sort", silent = true, buffer = bufNr }
-            )
-
-            vim.keymap.set(
-                { "n", "v" },
-                "<space>ir",
-                ":TSToolsRemoveUnusedImports<CR>",
-                {
-                    desc = "Imports remove unused",
-                    silent = true,
-                    buffer = bufNr,
-                }
-            )
-
-            vim.keymap.set(
-                { "n", "v" },
-                "<space>ia",
-                ":TSToolsAddMissingImports<CR>",
-                {
-                    desc = "Imports Add All missing",
-                    silent = true,
-                    buffer = bufNr,
-                }
-            )
-
-            vim.keymap.set(
-                { "n", "v" },
-                "<space>rf",
-                ":TSToolsRenameFile<CR>",
-                { desc = "Rename File", silent = true, buffer = bufNr }
-            )
-        end,
+    {
+        "dmmulroy/tsc.nvim",
+        opts = {
+            auto_open_qflist = false,
+        },
     },
 }
