9f94806a42a02db7b2c92ec7c60ccb1b196e7e5a
Carlos Gomes <carlos.algms@gmail.com>
2024-03-21 09:45:01 +0100

    Ignore LSP message

--- neovim/nvim/lua/plugins/notify.lua
+++ neovim/nvim/lua/plugins/notify.lua
@@ -5,7 +5,16 @@ return {
     },
     config = function()
         local notify = require("notify")
-        vim.notify = notify
+        local banned_messages = { "No information available" }
+
+        vim.notify = function(msg, ...)
+            for _, banned in ipairs(banned_messages) do
+                if msg == banned then
+                    return
+                end
+            end
+            return notify(msg, ...)
+        end
 
         ---@diagnostic disable-next-line: missing-fields
         notify.setup({
