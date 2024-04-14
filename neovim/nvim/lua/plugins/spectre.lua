833584ea364937589958811d0010c30e2870f717
Carlos Gomes <carlos.algms@gmail.com>
2024-04-14 18:31:50 +0200

    Add Spectre plugin to do project wide search and replace

---
+++ neovim/nvim/lua/plugins/spectre.lua
@@ -0,0 +1,31 @@
+return {
+    "nvim-pack/nvim-spectre",
+    denpendencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    keys = {
+        {
+            "<leader>S",
+            '<cmd>lua require("spectre").toggle()<CR>',
+            desc = "Toggle Spectre",
+        },
+        {
+            "<leader>sw",
+            '<cmd>lua require("spectre").open_visual({select_word=true})<CR>',
+            desc = "Search current word Spectre",
+        },
+        {
+            "<leader>sw",
+            '<esc><cmd>lua require("spectre").open_visual()<CR>',
+            desc = "Search current word Spectre",
+        },
+        {
+            "<leader>sp",
+            '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>',
+            desc = "Search on current file Spectre",
+        },
+    },
+    opts = {
+        is_block_ui_break = true, -- mapping backspace and enter key to avoid ui break
+    },
+}
