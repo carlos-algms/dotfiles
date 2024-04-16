566a727f14b7e7f12481d9448c6a3d4e211652b2
Carlos Gomes <carlos.algms@gmail.com>
2024-04-16 18:15:13 +0200

    Add comment type for GraphQL files

--- neovim/nvim/lua/plugins/commentary.lua
+++ neovim/nvim/lua/plugins/commentary.lua
@@ -1,7 +1,20 @@
 return {
     "tpope/vim-commentary",
-    -- config = function()
-    --     vim.api.nvim_set_keymap('n', '<leader>/', ':Commentary<CR>', { noremap = true, silent = true })
-    --     vim.api.nvim_set_keymap('v', '<leader>/', ':Commentary<CR>', { noremap = true, silent = true })
-    -- end
+    config = function()
+        -- https://github.com/tpope/vim-commentary?tab=readme-ov-file#faq
+        -- autocmd FileType apache setlocal commentstring=#\ %s
+        local commentaryGroup = vim.api.nvim_create_augroup(
+            "CommentaryAutoCmdGroup",
+            { clear = true }
+        )
+
+        vim.api.nvim_create_autocmd("FileType", {
+            group = commentaryGroup,
+            desc = "Set GraphQl comment string",
+            pattern = "graphql",
+            callback = function(ev)
+                vim.bo[ev.buf].commentstring = "# %s"
+            end,
+        })
+    end,
 }
