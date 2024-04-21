3cb6f84d6af9771874966f2f05e3eafe618e9a2d
Carlos Gomes <carlos.algms@gmail.com>
2024-04-21 11:54:50 +0200

    Switch from keys to keymap as it was crashing telescope

--- neovim/nvim/lua/plugins/oil.lua
+++ neovim/nvim/lua/plugins/oil.lua
@@ -1,5 +1,9 @@
 return {
     "stevearc/oil.nvim",
+    -- Optional dependencies
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
     opts = {
         -- https://github.com/stevearc/oil.nvim?tab=readme-ov-file#options
         view_options = {
@@ -9,15 +13,21 @@ return {
             ["q"] = "actions.close",
         },
     },
-    -- Optional dependencies
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
-    },
-    keys = {
-        {
+    config = function(_, opts)
+        require("oil").setup(opts)
+
+        vim.keymap.set(
+            "n",
             "<C-n>",
             "<cmd>Oil<CR>",
-            desc = "Revel current file in Oil",
-        },
-    },
+            { desc = "Revel current file in Oil" }
+        )
+    end,
+    -- keys = {
+    --     {
+    --         "<C-n>",
+    --         "<cmd>Oil<CR>",
+    --         desc = "Revel current file in Oil",
+    --     },
+    -- },
 }
