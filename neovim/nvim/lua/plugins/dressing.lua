ef006850c3e07cfdeb45da9a494c86a77404eb48
Carlos Gomes <carlos.algms@gmail.com>
2024-04-26 18:11:29 +0200

    Increse the input width and allow to enter normal mode with <Esc>

--- neovim/nvim/lua/plugins/dressing.lua
+++ neovim/nvim/lua/plugins/dressing.lua
@@ -4,6 +4,9 @@ return {
     opts = {
         input = {
             relative = "editor",
+            prefer_width = 0.5,
+            -- When true, <Esc> will close the modal
+            insert_only = false,
         },
     },
 }
