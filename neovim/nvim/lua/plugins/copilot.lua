5d21678571b7b840435ad0f7e8c40602b7fa2f75
Carlos Gomes <carlos.algms@gmail.com>
2024-05-09 11:28:27 +0200

    Enable Copilot Chat plugin

--- neovim/nvim/lua/plugins/copilot.lua
+++ neovim/nvim/lua/plugins/copilot.lua
@@ -1,42 +1,68 @@
 return {
-    "github/copilot.vim",
-    event = { "VeryLazy" },
-    keys = {
-        {
-            "<C-.>",
-            "<Plug>(copilot-accept-word)",
-            mode = "i",
-            desc = "Copilot accept word",
+    {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        branch = "canary",
+        dependencies = {
+            -- { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+            {
+                "github/copilot.vim",
+            },
+            { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
         },
-        {
-            "<C-;>",
-            "<Plug>(copilot-next)",
-            mode = "i",
-            desc = "Copilot next suggestion",
+        keys = {
+            {
+                "<leader>C",
+                "<cmd>CopilotChatOpen<CR>",
+                desc = "Open Copilot Chat",
+                mode = { "n", "v" },
+            },
         },
-        {
-            "<C-,>",
-            "<Plug>(copilot-suggest)",
-            mode = "i",
-            desc = "Copilot suggest",
+        opts = {
+            -- debug = true, -- Enable debugging
+            -- See Configuration section for rest
         },
+        -- See Commands section for default commands if you want to lazy load on them
     },
-    -- used init instead of config because copilot was holding Tab anyway
-    init = function()
-        -- I will use CMP mappings to have <Tab> fallback
-        vim.g.copilot_no_tab_map = true
-        vim.g.copilot_assume_mapped = true
-        vim.g.copilot_tab_fallback = ""
+    {
+        "github/copilot.vim",
+        event = { "VeryLazy" },
+        keys = {
+            {
+                "<C-.>",
+                "<Plug>(copilot-accept-word)",
+                mode = "i",
+                desc = "Copilot accept word",
+            },
+            {
+                "<C-;>",
+                "<Plug>(copilot-next)",
+                mode = "i",
+                desc = "Copilot next suggestion",
+            },
+            {
+                "<C-,>",
+                "<Plug>(copilot-suggest)",
+                mode = "i",
+                desc = "Copilot suggest",
+            },
+        },
+        -- used init instead of config because copilot was holding Tab anyway
+        init = function()
+            -- I will use CMP mappings to have <Tab> fallback
+            vim.g.copilot_no_tab_map = true
+            vim.g.copilot_assume_mapped = true
+            vim.g.copilot_tab_fallback = ""
 
-        vim.g.copilot_filetypes = {
-            yml = true,
-            yaml = true,
-            markdown = true,
-            gitcommit = true,
-            sagarename = false,
-            spectre_panel = false,
-            DressingInput = false,
-            oil = false,
-        }
-    end,
+            vim.g.copilot_filetypes = {
+                yml = true,
+                yaml = true,
+                markdown = true,
+                gitcommit = true,
+                sagarename = false,
+                spectre_panel = false,
+                DressingInput = false,
+                oil = false,
+            }
+        end,
+    },
 }
