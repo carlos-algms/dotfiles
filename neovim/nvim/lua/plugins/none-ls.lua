f26dcdd599f918bfdbd8054bd3f440abe883d4e1
Carlos Gomes <carlos.algms@gmail.com>
2024-05-25 15:00:37 +0200

    Automate intallation of cspell dictionary pt-br

--- neovim/nvim/lua/plugins/none-ls.lua
+++ neovim/nvim/lua/plugins/none-ls.lua
@@ -4,7 +4,36 @@ return {
         {
             "davidmh/cspell.nvim",
             -- build = "cd ~/.local/share/nvim/mason/packages/cspell && npm i @cspell/dict-pt-br",
-            build = "mkdir -p dictionaries && cd $_ && echo '{}' > package.json && npm i @cspell/dict-pt-br",
+            -- build = "mkdir -p dictionaries && cd $_ && echo '{}' > package.json && npm i @cspell/dict-pt-br",
+            init = function()
+                local folder_path = "~/.cache/nvim/cspell-dictionaries"
+
+                -- Check if the folder exists
+                local folder_exists = os.execute("test -d " .. folder_path)
+
+                -- If the folder doesn't exist, create it
+                if folder_exists ~= 0 then
+                    vim.notify(
+                        "Creating folder for cspell dictionaries",
+                        vim.log.levels.INFO
+                    )
+                    os.execute("mkdir -p " .. folder_path)
+
+                    local status, err = pcall(
+                        os.execute,
+                        "cd "
+                            .. folder_path
+                            .. " && echo '{}' > package.json && npm i @cspell/dict-pt-br"
+                    )
+
+                    if status ~= 0 then
+                        vim.notify(
+                            "Failed to install cspell dictionary " .. err,
+                            vim.log.levels.ERROR
+                        )
+                    end
+                end
+            end,
         },
     },
 
