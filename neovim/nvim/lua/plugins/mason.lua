9805657a627c3a83560e692740ca79039d66fd3b
Carlos Gomes <carlos.algms@gmail.com>
2024-06-03 09:42:58 +0200

    Replace neodev with lazydev - recommended by the author

--- neovim/nvim/lua/plugins/mason.lua
+++ neovim/nvim/lua/plugins/mason.lua
@@ -10,15 +10,18 @@ return {
         { "nvim-telescope/telescope.nvim" }, -- listing it here to import builtin safely
 
         {
-            -- apparently it should be installed before lspconfig
-            "folke/neodev.nvim",
-            -- TODO: enable it only for the nvim folder
-            -- enabled = false,
-            -- add lazy here as wasn't initializing all the time
-            lazy = false,
-            config = function()
-                require("neodev").setup({})
-            end,
+            "folke/lazydev.nvim",
+            ft = "lua", -- only load on lua files
+            opts = {
+                library = {
+                    -- Library items can be absolute paths
+                    -- "~/projects/my-awesome-lib",
+                    -- Or relative, which means they will be resolved as a plugin
+                    -- "LazyVim",
+                    -- When relative, you can also provide a path to the library in the plugin dir
+                    -- "luvit-meta/library", -- see below
+                },
+            },
         },
     },
 
