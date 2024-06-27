00836c9c7cd97e058be93362ad3d854663492415
Carlos Gomes <carlos.algms@gmail.com>
2024-06-27 10:05:35 +0200

    Add package.json plugin to show and update versions

---
+++ neovim/nvim/lua/plugins/nodejs.lua
@@ -0,0 +1,78 @@
+return {
+    {
+        "vuki656/package-info.nvim",
+
+        enabled = vim.g.has_node,
+
+        ft = "json",
+
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+        },
+
+        config = function()
+            local packageInfo = require("package-info")
+            packageInfo.setup({})
+
+            vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+                desc = "Set package.json keybindings",
+                pattern = { "package.json" },
+                callback = function(ev)
+                    -- Show dependency versions
+                    vim.keymap.set({ "n" }, "<leader>ns", packageInfo.show, {
+                        silent = true,
+                        buffer = ev.buf,
+                        desc = "Show package.json info",
+                    })
+
+                    -- Hide dependency versions
+                    vim.keymap.set({ "n" }, "<leader>nc", packageInfo.hide, {
+                        silent = true,
+                        buffer = ev.buf,
+                        desc = "Hide package.json info",
+                    })
+
+                    -- Toggle dependency versions
+                    vim.keymap.set({ "n" }, "<leader>nt", packageInfo.toggle, {
+                        silent = true,
+                        buffer = ev.buf,
+                        desc = "Toggle package.json info",
+                    })
+
+                    -- Update dependency on the line
+                    vim.keymap.set({ "n" }, "<leader>nu", packageInfo.update, {
+                        silent = true,
+                        buffer = ev.buf,
+                        desc = "Update current dependency line in package.json",
+                    })
+
+                    -- Delete dependency on the line
+                    vim.keymap.set({ "n" }, "<leader>nd", packageInfo.delete, {
+                        silent = true,
+                        buffer = ev.buf,
+                        desc = "Delete current dependency in package.json",
+                    })
+
+                    -- Install a new dependency
+                    vim.keymap.set({ "n" }, "<leader>ni", packageInfo.install, {
+                        silent = true,
+                        buffer = ev.buf,
+                        desc = "Install a new package.json dependency",
+                    })
+
+                    -- Install a different dependency version
+                    vim.keymap.set(
+                        { "n" },
+                        "<leader>np",
+                        packageInfo.change_version,
+                        {
+                            silent = true,
+                            buffer = ev.buf,
+                            desc = "Change the version of the current dependency line in package.json",
+                        }
+                    )
+                end,
+            })
+        end,
+    },
+}
