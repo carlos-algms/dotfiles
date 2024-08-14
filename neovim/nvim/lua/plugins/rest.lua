b2357f573f627423b6be1a9f5f15e8b262a69254
Carlos Gomes <carlos.algms@gmail.com>
2024-08-14 08:52:22 +0200

    Use Kulala instead of rest.nvim for REST client

--- neovim/nvim/lua/plugins/rest.lua
+++ neovim/nvim/lua/plugins/rest.lua
@@ -1,8 +1,81 @@
 return {
     {
+        -- I replaced rest.nvim because it has too many dependencies,
+        -- and luarocks was failing to install in the remote machines
+        "mistweaverco/kulala.nvim",
+
+        -- TODO: check if it works on remote machines
+        enabled = not vim.g.is_ssh,
+        ft = "http",
+        keys = {
+            {
+                "<leader>rc",
+                function()
+                    require("kulala").scratchpad()
+                end,
+                desc = "Open REST client scratchpad Kulala",
+                silent = true,
+            },
+        },
+
+        init = function()
+            vim.filetype.add({
+                extension = {
+                    ["http"] = "http",
+                },
+            })
+        end,
+
+        config = function()
+            -- Setup is required, even if you don't pass any options
+            local kulala = require("kulala")
+            kulala.setup()
+
+            vim.api.nvim_create_autocmd("FileType", {
+                group = vim.api.nvim_create_augroup("kulala", { clear = true }),
+                desc = "Set HTTP file keybindings",
+                pattern = { "http" },
+                callback = function(ev)
+                    vim.keymap.set("n", "[r", function()
+                        kulala.jump_prev()
+                    end, {
+                        desc = "Jump to previous request",
+                        silent = true,
+                        buffer = ev.buf,
+                    })
+
+                    vim.keymap.set("n", "]r", function()
+                        kulala.jump_next()
+                    end, {
+                        desc = "Jump to next request",
+                        silent = true,
+                        buffer = ev.buf,
+                    })
+
+                    vim.keymap.set("n", "<leader>rt", function()
+                        kulala.toggle_view()
+                    end, {
+                        desc = "Toggle response view body and headers",
+                        silent = true,
+                        buffer = ev.buf,
+                    })
+
+                    vim.keymap.set("n", "R", function()
+                        kulala.run()
+                    end, {
+                        desc = "Run request",
+                        silent = true,
+                        buffer = ev.buf,
+                    })
+                end,
+            })
+        end,
+    },
+    {
         "rest-nvim/rest.nvim",
         -- Disabling as it fails to install deps on remote machines
-        enabled = not vim.g.is_ssh,
+        -- enabled = not vim.g.is_ssh,
+        enabled = false,
         ft = "http",
         dependencies = {
             "nvim-neotest/nvim-nio",
