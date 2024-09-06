516fe7e34b7f9ede1b8d0db3fa47a6e79072a978
Carlos Gomes <carlos.algms@gmail.com>
2024-09-06 09:38:21 +0200

    Setup as kickstart.nvim suggests

--- neovim/nvim/lua/plugins/autopairs.lua
+++ neovim/nvim/lua/plugins/autopairs.lua
@@ -1,5 +1,13 @@
+-- https://github.com/nvim-lua/kickstart.nvim/blob/a22976111e406ec0e4903ae78bf66a1fc0125b8a/lua/kickstart/plugins/autopairs.lua
 return {
     "windwp/nvim-autopairs",
-    event = "VeryLazy",
-    opts = {}, -- this is equivalent to setup({}) function
+    event = "InsertEnter",
+    dependencies = { "hrsh7th/nvim-cmp" },
+    config = function()
+        require("nvim-autopairs").setup({})
+        -- If you want to automatically add `(` after selecting a function or method
+        local cmp_autoPairs = require("nvim-autopairs.completion.cmp")
+        local cmp = require("cmp")
+        cmp.event:on("confirm_done", cmp_autoPairs.on_confirm_done())
+    end,
 }
