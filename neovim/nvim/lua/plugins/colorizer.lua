ca772d6d16b9e5cfd385d794947849ce0cfb81e3
Carlos Gomes <carlos.algms@gmail.com>
2024-09-19 15:12:22 +0200

    Update config to enable hsl

--- neovim/nvim/lua/plugins/colorizer.lua
+++ neovim/nvim/lua/plugins/colorizer.lua
@@ -4,12 +4,24 @@ return {
     event = "VeryLazy",
     config = function()
         require("colorizer").setup({
-            "*",
+            "css",
+            "scss",
+            "javascript",
+            "javascriptreact",
+            "typescript",
+            "typescriptreact",
+            "html",
             "!telescope",
             "!dashboard",
             "!NvimTree",
             "!lazy",
             "!git",
+        }, {
+            mode = "background",
+            names = false,
+            css = true,
+            hsl_fn = true,
+            css_fn = true,
         })
     end,
 }
