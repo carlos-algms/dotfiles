d8f64e26ee4ed48571bf317602e87bc18a1a1172
Carlos Gomes <carlos.algms@gmail.com>
2024-09-27 13:28:11 +0200

    Add fdir alias to search for dir names instead of files

--- shell/common/aliases.sh
+++ shell/common/aliases.sh
@@ -4,11 +4,13 @@ alias path-show='echo $PATH | tr ":" "\n"'
 
 # Using which because it might change in Windows
 alias ff='`which -p find` . ! -path "**node_modules/**" ! -path "**.vscode/**" ! -path "**vendor/**" -type f -name '
+# consider using `fd -t d -H` instead
+alias fdir='find . \( -path "**/node_modules" -o -path "**/.git" \) -prune -o -type d -print'
 
 alias ack='ack --context=2 \
     --ignore-dir={.vscode,node_modules,vendor,.next,build,dist,coverage,storybook-static,.turbo} \
     --ignore-file=ext:{tsbuildinfo} \
-    --ignore-file=match:"/test|spec/" '
+    --ignore-file=match:"/test|spec|junit-results|json-results/" '
 
 alias yf="yarn --frozen-lockfile "
 alias yff="yf --force "
