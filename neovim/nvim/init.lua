ac00ffb58e0ab31c315551e327a2a5741141eeca
Carlos Gomes <carlos.algms@gmail.com>
2024-10-08 10:10:50 +0200

    Update Lazy setup to follow their Docs

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -1,16 +1,25 @@
+-- https://lazy.folke.io/installation
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({
         "git",
         "clone",
         "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
+        "--branch=stable",
+        lazyrepo,
         lazypath,
     })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
-
 vim.opt.rtp:prepend(lazypath)
 
 require("00_version_and_env")
@@ -20,6 +29,13 @@ require("my-darcula-theme")
 require("remap")
 require("commands")
 require("autocmds")
+
+vim.keymap.set(
+    { "n" },
+    "<leader>vl",
+    "<CMD>Lazy<CR>",
+    { desc = "Open Lazy plugin manager modal" }
+)
 
 require("lazy").setup("plugins", {
     change_detection = {
