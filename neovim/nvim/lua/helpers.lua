ce81ff2a8911218161df99d4aa7494b2920d94d3
Carlos Gomes <carlos.algms@gmail.com>
2024-10-16 22:49:35 +0200

    Adding types to autocomplete

--- neovim/nvim/lua/helpers.lua
+++ neovim/nvim/lua/helpers.lua
@@ -22,3 +22,36 @@ table.shallowMerge = function(t1, t2)
 
     return tNew
 end
+
+--- @alias AutoCmdEvent
+--- | "BufAdd"
+--- | "BufDelete"
+--- | "BufEnter"
+--- | "BufFilePost"
+--- | "BufFilePre"
+--- | "BufHidden"
+--- | "BufLeave"
+--- | "BufModifiedSet"
+--- | "BufNew"
+--- | "BufNewFile"
+--- | "BufRead"
+--- | "BufReadCmd"
+--- | "BufReadPost"
+--- | "BufReadPre"
+--- | "BufUnload"
+--- | "BufWinEnter"
+--- | "BufWinLeave"
+--- | "BufWipeout"
+--- | "BufWrite"
+--- | "BufWriteCmd"
+--- | "BufWritePost"
+--- | "BufWritePre"
+
+--- @class AutoCmdCallbackEvent
+--- @field public id number
+--- @field public event string
+--- @field public group number
+--- @field public match string
+--- @field public buf number
+--- @field public file string
+--- @field public data any
