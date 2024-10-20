8756f83aeca5091746de3ff060aec78b8b5fcfcb
Carlos Gomes <carlos.algms@gmail.com>
2024-10-21 00:01:43 +0200

    use native comment command

--- neovim/nvim/lua/plugins/commentary.lua
+++ neovim/nvim/lua/plugins/commentary.lua
@@ -1,31 +1,5 @@
 return {
     "tpope/vim-commentary",
     event = "VeryLazy",
-    -- enabled = not vim.g.is_nvim_0_10,
-    init = function()
-        -- https://github.com/tpope/vim-commentary?tab=readme-ov-file#faq
-        -- autocmd FileType apache setlocal commentstring=#\ %s
-        local commentaryGroup = vim.api.nvim_create_augroup(
-            "CommentaryAutoCmdGroup",
-            { clear = true }
-        )
-
-        vim.api.nvim_create_autocmd("FileType", {
-            group = commentaryGroup,
-            desc = "Set GraphQl comment string",
-            pattern = "graphql",
-            callback = function(ev)
-                vim.bo[ev.buf].commentstring = "# %s"
-            end,
-        })
-
-        vim.api.nvim_create_autocmd("FileType", {
-            group = commentaryGroup,
-            desc = "Set Prisma comment string",
-            pattern = "prisma",
-            callback = function(ev)
-                vim.bo[ev.buf].commentstring = "// %s"
-            end,
-        })
-    end,
+    enabled = not vim.g.is_nvim_0_10,
 }
