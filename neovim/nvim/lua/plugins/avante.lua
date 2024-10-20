857bdf8c4e2b0c2a4e61370ad9fd64b2f13f29ca
Carlos Gomes <carlos.algms@gmail.com>
2024-10-20 11:27:19 +0200

    Disable Avante in SSH

--- neovim/nvim/lua/plugins/avante.lua
+++ neovim/nvim/lua/plugins/avante.lua
@@ -1,6 +1,7 @@
 return {
     {
         "yetone/avante.nvim",
+        enabled = not vim.g.is_ssh,
         event = "VeryLazy",
         lazy = false,
         version = false, -- set this if you want to always pull the latest change
