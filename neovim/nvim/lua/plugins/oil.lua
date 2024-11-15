fee12d211f9f651b6ec304a41330e12624175efc
Carlos Gomes <carlos.algms@gmail.com>
2024-11-15 17:46:40 +0100

    Set same keymaps as Telescope for splits

--- neovim/nvim/lua/plugins/oil.lua
+++ neovim/nvim/lua/plugins/oil.lua
@@ -31,9 +31,26 @@ return {
         -- https://github.com/stevearc/oil.nvim?tab=readme-ov-file#options
         view_options = {
             show_hidden = true,
+            case_insensitive = false,
         },
         keymaps = {
             ["q"] = "actions.close",
+            ["<C-v>"] = {
+                "actions.select",
+                opts = { vertical = true, close = true },
+                desc = "Open the entry in a vertical split",
+            },
+            ["<C-x>"] = {
+                "actions.select",
+                opts = { horizontal = true, close = true },
+                desc = "Open the entry in a horizontal split",
+            },
+            -- Disabled to avoid mistyping
+            ["<C-s>"] = false,
+            ["<C-h>"] = false,
+            ["_"] = false,
+            ["`"] = false,
+            ["~"] = false,
         },
     },
     config = function(_, opts)
