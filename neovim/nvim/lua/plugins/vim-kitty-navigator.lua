8777cb8c95b66699137c1ba8491a20856a77d565
Carlos Gomes <carlos.algms@gmail.com>
2024-11-16 23:14:40 +0100

    Use IS_NVIM user_vars instead of process name in SSH

--- neovim/nvim/lua/plugins/vim-kitty-navigator.lua
+++ neovim/nvim/lua/plugins/vim-kitty-navigator.lua
@@ -2,4 +2,19 @@ return {
     "knubie/vim-kitty-navigator",
     -- Disabled to avoid exploit, check the files before copying them
     -- build = not vim.g.is_ssh and "cp ./*.py ~/.config/kitty/" or nil,
+    config = function()
+        if vim.g.is_ssh then
+            vim.fn.system("kitten @ set-user-vars IS_NVIM=true")
+
+            vim.api.nvim_create_autocmd("VimLeavePre", {
+                group = vim.api.nvim_create_augroup(
+                    "vim-kitty-navigator",
+                    { clear = true }
+                ),
+                callback = function()
+                    vim.fn.system("kitten @ set-user-vars IS_NVIM=false")
+                end,
+            })
+        end
+    end,
 }
