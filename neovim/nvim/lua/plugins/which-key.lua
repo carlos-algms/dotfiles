a780d26a0a865438184acf89f33dc1b456646a80
Carlos Gomes <carlos.algms@gmail.com>
2024-11-24 20:45:14 +0100

    Commenting unnecessary dependencies

--- neovim/nvim/lua/plugins/which-key.lua
+++ neovim/nvim/lua/plugins/which-key.lua
@@ -2,7 +2,7 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     dependencies = {
-        "ggandor/leap.nvim", -- add as dependency to try to avoid keymaps conflicts
+        -- "ggandor/leap.nvim", -- add as dependency to try to avoid keymaps conflicts
     },
     init = function()
         vim.o.timeout = true
