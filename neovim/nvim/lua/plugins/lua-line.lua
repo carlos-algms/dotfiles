b44b4875d36caa1578d784777bee07740ab2e299
Carlos Gomes <carlos.algms@gmail.com>
2024-11-24 05:38:29 +0100

    Add my own buffer name to have it in each window

--- neovim/nvim/lua/plugins/lua-line.lua
+++ neovim/nvim/lua/plugins/lua-line.lua
@@ -5,7 +5,62 @@ return {
         opt = true,
     },
     config = function()
-        -- local noice = require("noice")
+        local devIcons = require("nvim-web-devicons")
+        local highlight = require("lualine.highlight")
+        local utils = require("lualine.utils.utils")
+
+        local function buffer_name(self)
+            local relativePath = vim.fn.expand("%:~:.")
+
+            if relativePath == "" then
+                return "[No Name]"
+            end
+
+            local pathToRemove = vim.fn.fnamemodify(vim.fn.getcwd(), ":p")
+                .. ".git/:0:/"
+
+            relativePath = relativePath:gsub(pathToRemove, "")
+
+            local icon, icon_highlight_group = devIcons.get_icon(
+                vim.fn.expand("%:~:t"),
+                vim.fn.expand("#" .. vim.fn.bufnr("%") .. ":e")
+            )
+
+            if icon then
+                if not self.icon_hl_cache then
+                    self.icon_hl_cache = {}
+                end
+
+                -- https://github.com/nvim-lualine/lualine.nvim/blob/2a5bae925481f999263d6f5ed8361baef8df4f83/lua/lualine/components/filetype.lua#L47
+                local highlight_color =
+                    utils.extract_highlight_colors(icon_highlight_group, "fg")
+                if highlight_color then
+                    local default_highlight = self:get_default_hl()
+
+                    local icon_highlight = self.icon_hl_cache[highlight_color]
+                    if
+                        not icon_highlight
+                        or not highlight.highlight_exists(
+                            icon_highlight.name .. "_normal"
+                        )
+                    then
+                        icon_highlight = self:create_hl(
+                            { fg = highlight_color },
+                            icon_highlight_group
+                        )
+                        self.icon_hl_cache[highlight_color] = icon_highlight
+                    end
+
+                    icon = self:format_hl(icon_highlight)
+                        .. icon
+                        .. default_highlight
+                end
+
+                return icon .. " " .. relativePath
+            end
+
+            return relativePath
+        end
 
         local opts = {
             options = {
@@ -23,12 +78,34 @@ return {
                         -- "dapui_console",
                         -- "neo-tree",
                     },
+                    winbar = {
+                        "DiffviewFiles",
+                    },
                 },
             },
 
-            -- Winbar is one per window/split, and is only visible int he focused window
-            -- so, depnding to the width, the content will be truncated
-            -- winbar = {}
+            -- Winbar is one per window/split, and is only visible in he focused window
+            -- so, depending to the width, the content will be truncated
+            winbar = {
+                lualine_b = {
+                    {
+                        buffer_name,
+                        -- "filename",
+                        -- path = 1,
+                        separator = { left = " ", right = " " },
+                    },
+                },
+            },
+            inactive_winbar = {
+                lualine_b = {
+                    {
+                        buffer_name,
+                        -- "filename",
+                        -- path = 1,
+                        separator = { left = " ", right = " " },
+                    },
+                },
+            },
 
             -- Tab line is one per Tab pane, at the top, no matter how many windows/splits are in it
             -- tabline = {
@@ -57,17 +134,12 @@ return {
                     "branch",
                 },
                 lualine_c = {
-                    {
-                        "filename",
-                        path = 1,
-                    },
+                    -- {
+                    --     "filename",
+                    --     path = 1,
+                    -- },
                 },
                 lualine_x = {
-                    -- {
-                    --     noice.api.status.mode.get,
-                    --     cond = noice.api.status.mode.has,
-                    --     color = { fg = "#ff9e64" },
-                    -- },
                     -- "encoding",
                     "fileformat",
                 },
@@ -75,7 +147,7 @@ return {
                     "filetype",
                 },
                 lualine_z = {
-                    -- "location",
+                    "location",
                 },
             },
         }
