c566e2959e8b27fc54f023dfa308de6d17df1d91
Carlos Gomes <carlos.algms@gmail.com>
2024-11-27 10:00:20 +0100

    Remove the shallowMerge helper in favor of the deep_extend

--- neovim/nvim/lua/helpers/tables.lua
+++ neovim/nvim/lua/helpers/tables.lua
@@ -7,35 +7,12 @@ function M.deep_extend(target, source)
     for k, v in pairs(source) do
         if type(v) == "table" and type(target[k]) == "table" then
             M.deep_extend(target[k], v)
+        elseif type(k) == "number" then
+            table.insert(target, v)
         else
             target[k] = v
         end
     end
-end
-
----
---- Merge the second table into the first creating a new table, not modifying the originals.
----@param t1 table
----@param t2 table
-function M.shallowMerge(t1, t2)
-    local tNew = {}
-    for k, v in pairs(t1) do
-        if type(k) == "number" then
-            table.insert(tNew, v)
-        else
-            tNew[k] = v
-        end
-    end
-
-    for k, v in pairs(t2) do
-        if type(k) == "number" then
-            table.insert(tNew, v)
-        else
-            tNew[k] = v
-        end
-    end
-
-    return tNew
 end
 
 return M
