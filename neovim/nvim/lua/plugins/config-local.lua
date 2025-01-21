afebec6b1e0c6f3836a2b20d070dc30a0c71faf2
Carlos Gomes <carlos.algms@gmail.com>
2025-01-21 15:42:16 +0000

    Stop using fork as the original fixed the issue with lookup_parents

--- neovim/nvim/lua/plugins/config-local.lua
+++ neovim/nvim/lua/plugins/config-local.lua
@@ -1,8 +1,8 @@
 return {
-    -- "klen/nvim-config-local",
-    "mrjohannchang/nvim-config-local", -- the original seems to be abandoned
+    "klen/nvim-config-local",
+    -- "mrjohannchang/nvim-config-local", I added as this fixed the lookup_parents issue
+    -- version = "df334bb", -- fixing a specific hash to avoid exploits or malicious new code
     lazy = false,
-    version = "df334bb", -- fixing a specific hash to avoid exploits or malicious new code
     config = function()
         require("config-local").setup({
             -- Config file patterns to load (lua supported)
