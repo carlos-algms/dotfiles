209aca7536d1d05fcd79425803741d909a74f657
Carlos Gomes <carlos.algms@gmail.com>
2025-02-08 12:37:46 +0100

    Fix go property and methods color

--- neovim/nvim/after/queries/go/highlights.scm
+++ neovim/nvim/after/queries/go/highlights.scm
@@ -1,6 +1,8 @@
 ;; extends
 
 
+; it wasn't showing enum members as Constants
 (selector_expression
-  field: (field_identifier) @variable.member
+  ; reduced priority to avoid override function calls
+  field: (field_identifier) @variable.member (#set! "priority" 90)
   )
