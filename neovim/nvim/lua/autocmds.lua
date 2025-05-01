7a3be14fa008f15329d9741b8b8d679751b2050b
Carlos Gomes <carlos.algms@gmail.com>
2025-05-01 17:48:36 +0200

    Saving html Django filetype

--- neovim/nvim/lua/autocmds.lua
+++ neovim/nvim/lua/autocmds.lua
@@ -66,3 +66,8 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     pattern = { "docker-compose.yml", "docker-compose.yaml" },
     command = "set filetype=yaml.docker-compose",
 })
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = { "*.njk" },
+    command = "set filetype=htmldjango",
+})
