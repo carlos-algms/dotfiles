738cf3b43f7b8820cacaae13c860952bd199f394
Carlos Gomes <carlos.algms@gmail.com>
2025-05-29 16:48:18 +0200

    check for both lua executables to enable the plugin

--- neovim/nvim/lua/plugins/luarocks.lua
+++ neovim/nvim/lua/plugins/luarocks.lua
@@ -2,7 +2,8 @@ return {
     {
         "vhyrro/luarocks.nvim",
         priority = 2000, -- this plugin needs to run before anything else
-        enabled = vim.fn.executable("lua") == 1,
+        enabled = vim.fn.executable("lua") == 1
+            or vim.fn.executable("luajit") == 1,
         opts = {
             rocks = { "magick" },
         },
