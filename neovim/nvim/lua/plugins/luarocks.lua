ad89df6fd7ad179944c1ef976114c91e488987d7
Carlos Gomes <carlos.algms@gmail.com>
2025-05-31 00:59:46 +0200

    disable luarocks in ssh

--- neovim/nvim/lua/plugins/luarocks.lua
+++ neovim/nvim/lua/plugins/luarocks.lua
@@ -2,8 +2,11 @@ return {
     {
         "vhyrro/luarocks.nvim",
         priority = 2000, -- this plugin needs to run before anything else
-        enabled = vim.fn.executable("lua") == 1
-            or vim.fn.executable("luajit") == 1,
+        enabled = not vim.g.is_ssh
+            and (
+                vim.fn.executable("lua") == 1
+                or vim.fn.executable("luajit") == 1
+            ),
         opts = {
             rocks = { "magick" },
         },
