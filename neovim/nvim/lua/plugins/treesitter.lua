ffc61365749841e18ad368fb1a0cd5f663ce446b
Carlos Gomes <carlos.algms@gmail.com>
2025-06-14 01:14:01 +0200

    Add text objects for conditionals inner and outer

--- neovim/nvim/lua/plugins/treesitter.lua
+++ neovim/nvim/lua/plugins/treesitter.lua
@@ -58,6 +58,7 @@ return {
             },
             ignore_install = {},
             modules = {},
+
             textobjects = {
                 select = {
                     enable = true,
@@ -71,22 +72,26 @@ return {
                         -- You can use the capture groups defined in textobjects.scm
                         ["af"] = "@function.outer",
                         ["if"] = "@function.inner",
+
                         ["ac"] = "@class.outer",
-                        -- You can optionally set descriptions to the mappings (used in the desc parameter of
-                        -- nvim_buf_set_keymap) which plugins like which-key display
-                        ["ic"] = {
-                            query = "@class.inner",
-                            desc = "Select inner part of a class region",
-                        },
+
+                        ["ic"] = "@class.inner",
+
                         -- You can also use captures from other query groups like `locals.scm`
                         ["as"] = {
-                            query = "@scope",
+                            query = "@local.scope",
                             query_group = "locals",
                             desc = "Select language scope",
                         },
+
                         ["ab"] = "@block.outer",
+                        ["ib"] = "@block.inner",
+
+                        ["ii"] = "@conditional.inner",
+                        ["ai"] = "@conditional.outer",
                     },
                 },
+
                 move = {
                     enable = true,
                     set_jumps = true, -- whether to set jumps in the jumplist
