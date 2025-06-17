534d93e759f9f0312a874492048aed653d0a92d5
Carlos Gomes <carlos.algms@gmail.com>
2025-06-18 01:26:38 +0200

    Add Linux install script for utilities

--- shell/linux/bash/01_path_linux.sh
+++ shell/linux/bash/01_path_linux.sh
@@ -1,2 +1,6 @@
+PATH="$HOME/.local/bin:${DOTFILES_SHELL_PATH}/linux/bin:${DOTFILES_SHELL_PATH}/bin:$PATH"
 
-PATH="$HOME/bin:$HOME/.local/bin:${DOTFILES_SHELL_PATH}/linux/bin:${DOTFILES_SHELL_PATH}/bin:$PATH"
+FNM_PATH="$HOME/.local/share/fnm"
+if [ -d "$FNM_PATH" ]; then
+    PATH="'$FNM_PATH':$PATH"
+fi
--- shell/linux/install-linux.sh
+++ shell/linux/install-linux.sh
@@ -1,1 +1,29 @@
 #!/usr/bin/env bash
+
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+
+sudo apt update
+sudo apt install -y \
+    ack \
+    bat \
+    coreutils \
+    curl \
+    eza \
+    fd-find \
+    fzf \
+    git \
+    gpg \
+    htop \
+    luajit \
+    ripgrep \
+    tree \
+    watch \
+    wget \
+    xz-utils
+
+curl -fsSL https://fnm.vercel.app/install | bash -s -- --skip-shell
+curl -s https://ohmyposh.dev/install.sh | bash -s
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
