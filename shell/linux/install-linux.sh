7bfd8641711fdcae060631eeb43bc8500900d712
Carlos Gomes <carlos.algms@gmail.com>
2025-06-18 02:06:22 +0200

    Add install code for fzf, jq, and yazi

--- shell/linux/install-linux.sh
+++ shell/linux/install-linux.sh
@@ -13,10 +13,10 @@ sudo apt install -y \
     curl \
     eza \
     fd-find \
-    fzf \
     git \
     gpg \
     htop \
+    jq \
     luajit \
     ripgrep \
     tree \
@@ -27,3 +27,43 @@ sudo apt install -y \
 curl -fsSL https://fnm.vercel.app/install | bash -s -- --skip-shell
 curl -s https://ohmyposh.dev/install.sh | bash -s
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+
+mkdir -p $HOME/.local/bin
+
+ARCH=$(uname -m)
+
+# FZF most recent
+if [[ "$ARCH" == "x86_64" ]]; then
+    FZF_URL=$(curl -s https://api.github.com/repos/junegunn/fzf/releases/latest | grep browser_download_url | grep linux_amd64 | cut -d '"' -f 4)
+elif [[ "$ARCH" == "aarch64" ]]; then
+    FZF_URL=$(curl -s https://api.github.com/repos/junegunn/fzf/releases/latest | grep browser_download_url | grep linux_arm64 | cut -d '"' -f 4)
+else
+    echo "FZF Install: Unsupported architecture: $ARCH"
+fi
+
+if [[ -n "$FZF_URL" ]]; then
+    TMPDIR=$(mktemp -d)
+    curl -L "$FZF_URL" -o "$TMPDIR/fzf.tar.gz"
+    tar -xzf "$TMPDIR/fzf.tar.gz" -C "$TMPDIR"
+    mv "$TMPDIR/fzf" "$HOME/.local/bin/fzf"
+    chmod +x "$HOME/.local/bin/fzf"
+    rm -rf "$TMPDIR"
+fi
+
+# Install Yazi (latest release)
+if [[ "$ARCH" == "x86_64" ]]; then
+    YAZI_URL=$(curl -s https://api.github.com/repos/sxyazi/yazi/releases/latest | grep browser_download_url | grep yazi-x86_64-unknown-linux-gnu.zip | cut -d '"' -f 4)
+elif [[ "$ARCH" == "aarch64" ]]; then
+    YAZI_URL=$(curl -s https://api.github.com/repos/sxyazi/yazi/releases/latest | grep browser_download_url | grep yazi-aarch64-unknown-linux-gnu.zip | cut -d '"' -f 4)
+else
+    echo "Yazi Install: Unsupported architecture: $ARCH"
+fi
+
+if [[ -n "$YAZI_URL" ]]; then
+    TMPDIR=$(mktemp -d)
+    curl -L "$YAZI_URL" -o "$TMPDIR/yazi.zip"
+    unzip -j "$TMPDIR/yazi.zip" '*/yazi' -d "$TMPDIR"
+    mv "$TMPDIR/yazi" "$HOME/.local/bin/yazi"
+    chmod +x "$HOME/.local/bin/yazi"
+    rm -rf "$TMPDIR"
+fi
