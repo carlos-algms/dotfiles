123812b14dc785d9ce275479305971b9eb26f4b2
Carlos Gomes <carlos.algms@gmail.com>
2025-07-10 19:47:55 +0200

    Add Terraform LSP config to avoid crashing when opening diffview

---
+++ neovim/nvim/lsp/terraformls.lua
@@ -0,0 +1,17 @@
+--- @type vim.lsp.Config
+return {
+    workspace_required = true,
+
+    root_dir = function(bufnr, on_dir)
+        -- I had to validate the buffer name because terraform-ls was crashing saying it couldn't read the file
+
+        local bufname = vim.api.nvim_buf_get_name(bufnr) or ""
+
+        if bufname:find("diffview") then
+            return
+        end
+
+        local cwd = vim.uv.cwd()
+        on_dir(cwd)
+    end,
+}
