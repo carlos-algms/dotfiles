c54042019644612480375d7faeda750e91ef4a48
Carlos Gomes <carlos.algms@gmail.com>
2025-08-05 09:51:09 +0200

    make the selectors more specific to avoid injecting into list items

--- neovim/nvim/after/queries/markdown/injections.scm
+++ neovim/nvim/after/queries/markdown/injections.scm
@@ -1,13 +1,15 @@
 ;; extends
 ; inject TSX into inline nodes starting with import/export
 ((inline) @injection.content
-          (#lua-match? @injection.content "^%s*import")
-          (#set! injection.include-children)
-          (#set! injection.language "typescript")
-          )
+  (#lua-match? @injection.content "^%s*import")
+  (#not-has-ancestor? @injection.content list_item)
+  (#set! injection.include-children)
+  (#set! injection.language "typescript")
+  )
 
 ((inline) @injection.content
-          (#lua-match? @injection.content "^%s*export")
-          (#set! injection.include-children)
-          (#set! injection.language "typescript")
-          )
+  (#lua-match? @injection.content "^%s*export")
+  (#not-has-ancestor? @injection.content list_item)
+  (#set! injection.include-children)
+  (#set! injection.language "typescript")
+  )
