d5d663bbc445f4775091604b4b358ced34488620
Carlos Gomes <carlos.algms@gmail.com>
2025-08-08 09:01:36 +0100

    Add native quickfix list manipulation (add/remove entries)

--- neovim/nvim/lua/plugins/trouble.lua
+++ neovim/nvim/lua/plugins/trouble.lua
@@ -62,6 +62,60 @@ return {
         },
     },
 
+    init = function()
+        vim.keymap.set("n", "<leader>q", function()
+            local line = vim.fn.getline(".")
+            local trimmed = vim.trim(line)
+
+            vim.fn.setqflist({
+                {
+                    filename = vim.fn.expand("%"),
+                    lnum = vim.fn.line("."),
+                    col = vim.fn.col("."),
+                    text = trimmed,
+                },
+            }, "a")
+        end, { desc = "Add current file to quickfix" })
+
+        vim.keymap.set(
+            "n",
+            "<leader>qo",
+            "<CMD>copen<CR>",
+            {
+                silent = true,
+                noremap = true,
+                desc = "Open the native quickFix list",
+            }
+        )
+
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "qf",
+            callback = function(event)
+                vim.keymap.set("n", "dd", function()
+                    local qflist = vim.fn.getqflist()
+                    if #qflist == 0 then
+                        return
+                    end
+
+                    local line = vim.fn.line(".")
+                    table.remove(qflist, line)
+                    vim.fn.setqflist({}, " ", { items = qflist })
+
+                    local new_count = #qflist
+
+                    if new_count > 0 then
+                        local new_line = math.min(line, new_count)
+                        vim.api.nvim_win_set_cursor(0, { new_line, 0 })
+                    end
+                end, {
+                    buffer = event.buf,
+                    silent = true,
+                    desc = "Delete entry in the quickfix list",
+                })
+            end,
+        })
+    end,
+
     config = function()
         require("trouble").setup({
             auto_refresh = false,
