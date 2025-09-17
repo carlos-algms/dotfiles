837a2ecbbdac76da837e85b068eada5a62f77a1b
Carlos Gomes <carlos.algms@gmail.com>
2025-09-17 18:08:17 +0200

    Add some tab management maps

--- neovim/nvim/lua/remap.lua
+++ neovim/nvim/lua/remap.lua
@@ -1,115 +1,139 @@
-vim.keymap.set(
+local setKeymap = vim.keymap.set
+
+setKeymap(
     { "n", "v" },
     "<leader>vi",
     "<CMD>Inspect<CR>",
     { desc = "Inspect current highlight" }
 )
 
-vim.keymap.set(
+setKeymap(
     { "n", "v" },
     "<leader>vI",
     "<CMD>InspectTree<CR>",
     { desc = "Show Inspect Tree" }
 )
 
-vim.keymap.set(
+setKeymap(
     { "n" },
     "<leader>vr",
     "<cmd>let &relativenumber = !&relativenumber<CR>",
     { desc = "Toggle relative line numbers globally" }
 )
 
-vim.keymap.set({ "n" }, "<leader>vw", function()
+setKeymap({ "n" }, "<leader>vw", function()
     vim.wo.wrap = not vim.wo.wrap
 end, { desc = "Toggle line wrapping locally" })
 
-vim.keymap.set("v", [[a"]], [[2i"]], { desc = "Select double quoted text" })
-vim.keymap.set("v", [[a']], [[2i']], { desc = "Select single quoted text" })
+setKeymap("v", [[a"]], [[2i"]], { desc = "Select double quoted text" })
+setKeymap("v", [[a']], [[2i']], { desc = "Select single quoted text" })
+
+-- Tabs management
+setKeymap(
+    "n",
+    "<leader>vtn",
+    "<cmd>tabnew<cr>",
+    { desc = "Open new tab", silent = true }
+)
+setKeymap(
+    "n",
+    "<leader>vtc",
+    "<cmd>tabclose<cr>",
+    { desc = "Close current tab", silent = true }
+)
+setKeymap(
+    "n",
+    "<leader>vts",
+    "<cmd>tab split<cr>",
+    { desc = "Open current buffer in a new tab", silent = true }
+)
 
 -- Keeps the cursor on the center of the screen when running Ctrl + D or U
-vim.keymap.set(
+setKeymap(
     "n",
     "<C-d>",
     "<C-d>zz",
     { desc = "Scroll down half page and center the cursor on the screen" }
 )
-vim.keymap.set(
+setKeymap(
     "n",
     "<C-u>",
     "<C-u>zz",
     { desc = "Scroll up half page and center the cursor on the screen" }
 )
 
+setKeymap(
+    { "n", "v" },
+    "gf",
+    "gF",
+    { desc = "Go to file under cursor including line numbers" }
+)
+
 -- Keeps the cursor on the center when moving to next/prev search results
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+setKeymap("n", "n", "nzzzv")
+setKeymap("n", "N", "Nzzzv")
 
 -- copy to system clipboard
-vim.keymap.set(
+setKeymap(
     { "n", "v" },
     "<leader>y",
     [["+y]],
     { desc = "Copy to system clipboard" }
 )
 
-vim.keymap.set(
+setKeymap(
     { "n", "v" },
     "<leader>Y",
     [["+yy]],
     { desc = "Copy entire line to system clipboard" }
 )
 
-vim.keymap.set("n", "Q", "<nop>")
+setKeymap("n", "Q", "<nop>")
 
-vim.keymap.set(
-    { "i" },
-    "<C-j>",
-    "<C-o>o",
-    { desc = "Insert a new line bellow" }
-)
+setKeymap({ "i" }, "<C-j>", "<C-o>o", { desc = "Insert a new line bellow" })
 
-vim.keymap.set({ "i" }, "<C-k>", "<C-o>O", { desc = "Insert a new line above" })
+setKeymap({ "i" }, "<C-k>", "<C-o>O", { desc = "Insert a new line above" })
 
 -- better indenting
-vim.keymap.set("v", "<", "<gv")
-vim.keymap.set("v", ">", ">gv")
+setKeymap("v", "<", "<gv")
+setKeymap("v", ">", ">gv")
 
-vim.keymap.set(
+setKeymap(
     { "n", "i" },
     "<C-S-Up>",
     "<cmd>resize +2<cr>",
     { desc = "Increase window height" }
 )
 
-vim.keymap.set(
+setKeymap(
     { "n", "i" },
     "<C-S-Down>",
     "<cmd>resize -2<cr>",
     { desc = "Decrease window height" }
 )
 
-vim.keymap.set(
+setKeymap(
     { "n", "i" },
     "<C-S-Left>",
     "<cmd>vertical resize -2<cr>",
     { desc = "Decrease window width" }
 )
 
-vim.keymap.set(
+setKeymap(
     { "n", "i" },
     "<C-S-Right>",
     "<cmd>vertical resize +2<cr>",
     { desc = "Increase window width" }
 )
 
-vim.keymap.set(
+setKeymap(
     { "n", "i", "v" },
     "<D-s>",
     "<esc><cmd>update<cr>",
     { desc = "Save current buffer" }
 )
 
-vim.keymap.set(
+setKeymap(
     { "n", "i" },
     "<D-w>",
     "<esc><cmd>close<cr>",
@@ -117,12 +141,12 @@ vim.keymap.set(
 )
 
 -- Disabled to sue vim-kitty-navigator
--- vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
--- vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
--- vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
--- vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
+-- set("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
+-- set("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
+-- set("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
+-- set("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
 
-vim.keymap.set("n", "<Esc>", function()
+setKeymap("n", "<Esc>", function()
     if vim.opt.hlsearch then
         vim.cmd.nohl()
     else
