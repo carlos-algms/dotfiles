26e2d84274ecac8ea8c0b716922a2043ae85157a
Carlos Gomes <carlos.algms@gmail.com>
2025-09-30 20:07:00 +0200

    Add git branch delete gone alias - bgdg

--- shell/common/aliases_git.sh
+++ shell/common/aliases_git.sh
@@ -1,5 +1,9 @@
 # Remove all branches except for the master
-alias gbda='git branch | grep -v $(git_main_branch) | xargs git branch -D'
+# I'm disabling this for now, as it's dangerous
+# alias gbda='git branch | grep -v $(git_main_branch) | xargs git branch -D'
+
+# Delete all local branches that have been removed from the remote (gone)
+alias gbdg="git branch -vv | grep ': gone]' | awk '{print \$1}' | tee /dev/stderr | xargs git branch -D"
 
 # override git commit command to remove --verbose
 ## commenting for now to test Copilot suggestions
