652198b56dd6d7c7dd6d722b82ad9d396fc91eab
Carlos Gomes <carlos.algms@gmail.com>
2025-10-11 13:06:33 +0200

    disable eslint and cspell for ignored patterns

--- neovim/nvim/lsp/eslint.lua
+++ neovim/nvim/lsp/eslint.lua
@@ -1,5 +1,9 @@
+local forbidden_folders = { "node_modules", "dist", "build", "vendor" }
+
 ---@type vim.lsp.Config
 return {
+    workspace_required = true,
+
     root_markers = {
         ".eslintrc.js",
         ".eslintrc.json",
@@ -13,6 +17,23 @@ return {
         "node_modules/",
         "package.json",
     },
+
+    root_dir = function(bufnr, on_dir)
+        -- I had to validate the buffer name because eslint-ls
+        -- was crashing when running inside node_modules
+
+        local bufname = vim.api.nvim_buf_get_name(bufnr) or ""
+
+        for _, folder in ipairs(forbidden_folders) do
+            if bufname:find(folder, 1, true) then
+                return
+            end
+        end
+
+        local cwd = vim.uv.cwd()
+        on_dir(cwd)
+    end,
+
     settings = {
         options = {
             -- Disabled, as if a config file is found, it won't start. and this is different depending o eslint version
