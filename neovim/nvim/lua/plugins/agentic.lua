38a01b42778714cc13750764db2ac656b4ee3d40
Carlos Gomes <carlos.algms@gmail.com>
2025-11-29 23:09:27 +0100

    only enable agentic local folder if not in ssh session

--- neovim/nvim/lua/plugins/agentic.lua
+++ neovim/nvim/lua/plugins/agentic.lua
@@ -1,79 +1,76 @@
 local M = {
-    {
-        dir = vim.uv.os_homedir() .. "/projects/agentic.nvim",
-        dev = true,
-        name = "agentic.nvim",
 
-        "carlos-algms/agentic.nvim",
+    "carlos-algms/agentic.nvim",
 
-        event = "VeryLazy",
+    event = "VeryLazy",
 
-        version = false,
+    version = false,
 
-        opts = function(_, _maybeOpts)
-            local claudeKeyName = "CARLOS_ANTHROPIC_API_KEY"
+    opts = function(_, _maybeOpts)
+        local claudeKeyName = "CARLOS_ANTHROPIC_API_KEY"
 
-            ---@module 'agentic'
-            ---@type agentic.UserConfig
-            local config = {
-                debug = true,
+        ---@module 'agentic'
+        ---@type agentic.UserConfig
+        local config = {
+            debug = true,
 
-                --- @type agentic.UserConfig.ProviderName
-                provider = "claude-acp",
+            --- @type agentic.UserConfig.ProviderName
+            provider = "claude-acp",
 
-                acp_providers = {
-                    ["claude-acp"] = {
-                        command = "claude-code-acp",
-                        env = {
-                            ANTHROPIC_API_KEY = os.getenv(claudeKeyName),
-                        },
+            acp_providers = {
+                ["claude-acp"] = {
+                    command = "claude-code-acp",
+                    env = {
+                        ANTHROPIC_API_KEY = os.getenv(claudeKeyName),
                     },
                 },
-            }
-
-            return config
-        end,
-
-        keys = {
-            {
-                "<C-\\>",
-                function()
-                    require("agentic").toggle()
-                end,
-                desc = "Agentic Open",
-                silent = true,
-                mode = { "n", "v", "i" },
             },
+        }
 
-            {
-                "<C-'>",
-                function()
-                    require("agentic").add_selection_or_file_to_context()
-                end,
-                desc = "Agentic Add Selection to context",
-                silent = true,
-                mode = { "n", "v" },
-            },
+        return config
+    end,
 
-            {
-                "<C-,>",
-                function()
-                    require("agentic").new_session()
-                end,
-                desc = "Agentic New Session",
-                silent = true,
-                mode = { "n", "v", "i" },
-            },
+    keys = {
+        {
+            "<C-\\>",
+            function()
+                require("agentic").toggle()
+            end,
+            desc = "Agentic Open",
+            silent = true,
+            mode = { "n", "v", "i" },
         },
 
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-            "nvim-lua/plenary.nvim",
-            "MunifTanjim/nui.nvim",
-            "folke/snacks.nvim",
-            "nvim-tree/nvim-web-devicons",
+        {
+            "<C-'>",
+            function()
+                require("agentic").add_selection_or_file_to_context()
+            end,
+            desc = "Agentic Add Selection to context",
+            silent = true,
+            mode = { "n", "v" },
+        },
+
+        {
+            "<C-,>",
+            function()
+                require("agentic").new_session()
+            end,
+            desc = "Agentic New Session",
+            silent = true,
+            mode = { "n", "v", "i" },
         },
     },
+
+    dependencies = {
+        -- "nvim-tree/nvim-web-devicons",
+    },
 }
+
+if not vim.g.is_ssh then
+    M.dir = vim.uv.os_homedir() .. "/projects/agentic.nvim"
+    M.dev = true
+    M.name = "agentic.nvim"
+end
 
 return M
