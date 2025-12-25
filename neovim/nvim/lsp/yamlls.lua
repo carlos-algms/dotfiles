26f549cd4f979ee74312fc2a3b4db4a7de985e03
Carlos Gomes <carlos.algms@gmail.com>
2025-12-25 15:38:23 +0100

    add check to only start yaml ls if the file exists, same error as others

--- neovim/nvim/lsp/yamlls.lua
+++ neovim/nvim/lsp/yamlls.lua
@@ -1,9 +1,22 @@
 --- @type vim.lsp.Config
 return {
+    root_dir = function(bufnr, on_dir)
+        local bufname = vim.api.nvim_buf_get_name(bufnr) or ""
+
+        if bufname == "" or vim.uv.fs_stat(bufname) == nil then
+            return
+        end
+
+        local cwd = vim.uv.cwd()
+        on_dir(cwd)
+    end,
+
     before_init = function(_, client_config)
+        ---@diagnostic disable-next-line: inject-field
         client_config.settings.yaml.schemas =
             require("schemastore").yaml.schemas()
     end,
+
     settings = {
         redhat = { telemetry = { enabled = false } },
         yaml = {
