a21fbba3c67c4bf26aa978f1102d57e3ae0df45e
Carlos Gomes <carlos.algms@gmail.com>
2026-05-07 13:54:34 +0200

    Add `y` alias to Yazi as their Docs suggest - keeps current cwd on exit

--- shell/common/functions.sh
+++ shell/common/functions.sh
@@ -1,26 +1,36 @@
 # Easy extract
-extract () {
-    if [ -f $1 ] ; then
-        filename=$(basename -- "$1")
-        filename="${filename%.*}"
-        target="${2-"./$filename"}"
+extract() {
+  if [ -f $1 ]; then
+    filename=$(basename -- "$1")
+    filename="${filename%.*}"
+    target="${2-"./$filename"}"
 
-        case $1 in
-            *.tar.bz2)   tar xvjf $1 -C "$target"   ;;
-            *.tar.gz)    tar xvzf $1 -C "$target"   ;;
-            *.tar.xz)    tar xJf $1 -C "$target"    ;;
-            *.bz2)       bunzip2 $1                 ;;
-            *.rar)       rar x $1 "$target"         ;;
-            *.gz)        gunzip $1                  ;;
-            *.tar)       tar xvf $1  -C "$target"   ;;
-            *.tbz2)      tar xvjf $1 -C "$target"   ;;
-            *.tgz)       tar xvzf $1 -C "$target"   ;;
-            *.zip)       unzip $1 -d "$target"      ;;
-            *.Z)         uncompress $1              ;;
-            *.7z)        7z x $1 -o"$target"        ;;
-            *)           echo "don't know how to extract '$1'..." ;;
-        esac
-    else
-        echo "'$1' is not a valid file!"
-    fi
+    case $1 in
+    *.tar.bz2) tar xvjf $1 -C "$target" ;;
+    *.tar.gz) tar xvzf $1 -C "$target" ;;
+    *.tar.xz) tar xJf $1 -C "$target" ;;
+    *.bz2) bunzip2 $1 ;;
+    *.rar) rar x $1 "$target" ;;
+    *.gz) gunzip $1 ;;
+    *.tar) tar xvf $1 -C "$target" ;;
+    *.tbz2) tar xvjf $1 -C "$target" ;;
+    *.tgz) tar xvzf $1 -C "$target" ;;
+    *.zip) unzip $1 -d "$target" ;;
+    *.Z) uncompress $1 ;;
+    *.7z) 7z x $1 -o"$target" ;;
+    *) echo "don't know how to extract '$1'..." ;;
+    esac
+  else
+    echo "'$1' is not a valid file!"
+  fi
 }
+
+if command -v yazi >/dev/null 2>&1; then
+  function y() {
+    local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+    command yazi "$@" --cwd-file="$tmp"
+    IFS= read -r -d '' cwd <"$tmp"
+    [ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+    rm -f -- "$tmp"
+  }
+fi
